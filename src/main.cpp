7b224ad26b4fc54d5cc7f7cbe2ef0b6839816c85
sqiu <sqiu@student.42vienna.com>
2024-06-02 18:49:09 +0200

    feat: add main

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdexcept>
+#include "Server.hpp"
+
+int main(int argc, char** argv)
+{
+	if (argc != 2) {
+		std::cerr << "error: arguments invalid\n";
+		return 1;
+	}
+	(void)argv;
+	try{
+		Server	webserv;
+		webserv.run();
+	}
+	catch (std::exception& e){
+		std::cerr << "error" << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
