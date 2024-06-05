861d7a5cf58cd02c2a8dbbd175d54887702ffc70
QCHR1581 <chris1581.at@gmail.com>
2024-06-05 17:39:57 +0000

    refactor: replaced classes by structs

--- inc/ConfigFile.hpp
+++ inc/ConfigFile.hpp
@@ -1,14 +1,35 @@
 #pragma once
 
-#include "ServerConfig.hpp"
+#include <iostream>
+#include <map>
+#include <vector>
 
-class ConfigFile {
-public:
-    ConfigFile();
-    ~ConfigFile();
+enum Method { MethodGet,
+    MethodPost,
+    MethodPut,
+    MethodDelete,
+    MethodCount };
 
-private:
-    std::vector<ServerConfig> m_servers_config;
+struct Location {
+    const std::string m_path;
+    const std::string m_root;
+    const std::string m_index;
+    const std::string m_cgiExt;
+    const std::string m_cgiPath;
+    const bool m_autoindex;
+    bool m_allowed_methods[MethodCount];
+    std::map<unsigned short, std::string> m_return;
 };
 
-#endif
+struct ServerConfig {
+    const std::string m_serverName;
+    const std::string m_host;
+    const unsigned short m_port;
+    const unsigned long m_maxBodySize;
+    std::map<unsigned short, std::string> m_errorPage;
+    std::vector<Location> m_locations;
+};
+
+struct ConfigFile {
+    std::vector<ServerConfig> m_serverConfigs;
+};
