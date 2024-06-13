3b71a3881ef4d0f082047ff99b3daf77092ea3d6
sirui <sirui.qiu01@gmail.com>
2024-06-13 14:29:08 +0200

    test: add further valid request line tests

--- src/test.cpp
+++ src/test.cpp
@@ -72,6 +72,10 @@ void	testValidRequestLine() {
 	std::pair<std::string, std::string>	tests[] = {
 		std::make_pair("GET /search?query=openai&year=2024#conclusion HTTP/1.1\r\n"
 			, "GET\n/search\nquery=openai&year=2024\nconclusion\n1.1"),
+		std::make_pair("DELETE /index.html HTTP/1.1\r\n"
+			, "DELETE\n/index.html\n\n\n1.1"),
+		std::make_pair("POST /abracadabra/ipsum?user=aziz&key=password HTTP/2.0\r\n"
+			, "POST\n/abracadabra/ipsum\nuser=aziz&key=password\n\n2.0"),
 	};
 	runTests("VALID REQUEST LINES", sizeof(tests) / sizeof(tests[0]), tests);
 }
