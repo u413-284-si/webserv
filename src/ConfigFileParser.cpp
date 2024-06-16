a8075cf3917342ff9fac5f8cdaaebdefdf3facbb
QCHR1581 <chris1581.at@gmail.com>
2024-06-16 14:20:38 +0000

    style: formatted ConfigFileParser cpp file

--- src/ConfigFileParser.cpp
+++ src/ConfigFileParser.cpp
@@ -1,75 +1,70 @@
 #include "ConfigFileParser.hpp"
 
-
 ConfigFileParser::ConfigFileParser(const std::string& configFilePath)
 {
-	std::ifstream inputConfigFile;
-	std::string configFileLine;
+    std::ifstream inputConfigFile;
+    std::string configFileLine;
 
-	inputConfigFile.open(configFilePath.c_str());
-	if (!inputConfigFile)
-		throw std::runtime_error("Error: Failed to open config file");
-	else if (inputConfigFile.peek() == std::ifstream::traits_type::eof())
-		throw std::runtime_error("Error: Config file is empty");
+    inputConfigFile.open(configFilePath.c_str());
+    if (!inputConfigFile)
+        throw std::runtime_error("Error: Failed to open config file");
+    else if (inputConfigFile.peek() == std::ifstream::traits_type::eof())
+        throw std::runtime_error("Error: Config file is empty");
 
-	getline(inputConfigFile, configFileLine);
-	if (removeLeadingAndTrailingSpaces(configFileLine) != "http {")
-		throw std::runtime_error("Error: Config file does not start with 'http {'");
-	checkBrackets(configFileLine);
+    getline(inputConfigFile, configFileLine);
+    if (removeLeadingAndTrailingSpaces(configFileLine) != "http {")
+        throw std::runtime_error("Error: Config file does not start with 'http {'");
+    checkBrackets(configFileLine);
 
-	while (getline(inputConfigFile, configFileLine))
-	{
-		checkBrackets(configFileLine);
-		configFileLine = removeLeadingAndTrailingSpaces(configFileLine);
-		if (configFileLine == "server {")
-		{
-			getline(inputConfigFile, configFileLine);
-			configFileLine = removeLeadingAndTrailingSpaces(configFileLine);	
-			readServerConfig(configFileLine);
-		}
-	}
+    while (getline(inputConfigFile, configFileLine)) {
+        checkBrackets(configFileLine);
+        configFileLine = removeLeadingAndTrailingSpaces(configFileLine);
+        if (configFileLine == "server {") {
+            getline(inputConfigFile, configFileLine);
+            configFileLine = removeLeadingAndTrailingSpaces(configFileLine);
+            readServerConfig(configFileLine);
+        }
+    }
 
-	if (m_brackets.size() != 0)
-		throw std::runtime_error("Error: Missing bracket(s) in config file");
+    if (m_brackets.size() != 0)
+        throw std::runtime_error("Error: Missing bracket(s) in config file");
 }
 
-ConfigFileParser::~ConfigFileParser(){}
+ConfigFileParser::~ConfigFileParser() { }
 
-void ConfigFileParser::checkBrackets(const std::string &configFileLine)
+void ConfigFileParser::checkBrackets(const std::string& configFileLine)
 {
-	for (std::string::const_iterator it = configFileLine.begin(); it != configFileLine.end(); it++)
-	{
-		if (*it == '{')
-			m_brackets.push('{');
-		else if (*it == '}')
-			m_brackets.pop();
-	}
-
+    for (std::string::const_iterator it = configFileLine.begin(); it != configFileLine.end(); it++) {
+        if (*it == '{')
+            m_brackets.push('{');
+        else if (*it == '}')
+            m_brackets.pop();
+    }
 }
 
-std::string ConfigFileParser::removeLeadingAndTrailingSpaces(const std::string &str)
+std::string ConfigFileParser::removeLeadingAndTrailingSpaces(const std::string& str)
 {
-	std::string result;
-	
-	result = str;
-	result.erase(0, str.find_first_not_of(' '));
-	result.erase(result.find_last_not_of(' ') + 1);
+    std::string result;
 
-	return (result);
+    result = str;
+    result.erase(0, str.find_first_not_of(' '));
+    result.erase(result.find_last_not_of(' ') + 1);
+
+    return (result);
 }
 
-void ConfigFileParser::readServerConfig(const std::string &configFileLine)
+void ConfigFileParser::readServerConfig(const std::string& configFileLine)
 {
-	ServerConfig serverConfig;
-	std::string directive;
-	const char* validServerDirectives[] = {"server_name", "listen", "host", "client_max_body_size", "error_page", "location", "root", "location"};
-	const int validServerDirectivesSize = sizeof(validServerDirectives) / sizeof(validServerDirectives[0]);
-	
-	std::set<std::string> validServerDirectivesSet(validServerDirectives, validServerDirectives + validServerDirectivesSize);
+    ServerConfig serverConfig;
+    std::string directive;
+    const char* validServerDirectives[] = { "server_name", "listen", "host", "client_max_body_size", "error_page", "location", "root", "location" };
+    const int validServerDirectivesSize = sizeof(validServerDirectives) / sizeof(validServerDirectives[0]);
 
-	directive = configFileLine.substr(0, configFileLine.find(' '));
-	if (std::find(validServerDirectivesSet.begin(), validServerDirectivesSet.end(), directive) == validServerDirectivesSet.end())
-		throw std::runtime_error("Error: Invalid server directive");
+    std::set<std::string> validServerDirectivesSet(validServerDirectives, validServerDirectives + validServerDirectivesSize);
 
-	m_configFile.serverConfigs.push_back(serverConfig);
+    directive = configFileLine.substr(0, configFileLine.find(' '));
+    if (std::find(validServerDirectivesSet.begin(), validServerDirectivesSet.end(), directive) == validServerDirectivesSet.end())
+        throw std::runtime_error("Error: Invalid server directive");
+
+    m_configFile.serverConfigs.push_back(serverConfig);
 }
