ad4c28a4eba93b26eaa989b302d3150e6050bfff
QCHR1581 <chris1581.at@gmail.com>
2024-06-16 14:09:35 +0000

    feat: check i opening of config file fails or if the file is empty

--- src/ConfigFileParser.cpp
+++ src/ConfigFileParser.cpp
@@ -3,7 +3,14 @@
 
 ConfigFileParser::ConfigFileParser(const std::string& configFilePath)
 {
+	std::ifstream inputConfigFile;
+	std::string configFileLine;
 
+	inputConfigFile.open(configFilePath.c_str());
+	if (!inputConfigFile)
+		throw std::runtime_error("Error: Failed to open config file");
+	else if (inputConfigFile.peek() == std::ifstream::traits_type::eof())
+		throw std::runtime_error("Error: Config file is empty");
 }
 
 ConfigFileParser::~ConfigFileParser()
