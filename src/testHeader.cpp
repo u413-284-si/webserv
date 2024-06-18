34a909a80ec161a10b3fb57680d5551a5582832f
sirui <sirui.qiu01@gmail.com>
2024-06-18 19:19:37 +0200

    test: \r at end of header lines

--- src/testHeader.cpp
+++ src/testHeader.cpp
@@ -36,9 +36,9 @@ void	runHeaderTests(const std::string& name
 void	testValidHeader()
 {
 	std::pair<std::string, std::string>	tests[] = {
-		std::make_pair("GET /search?query=openai&year=2024#conclusion HTTP/1.1\r\nHost: www.example.com\nUser-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\nAccept-Language: en, mi"
+		std::make_pair("GET /search?query=openai&year=2024#conclusion HTTP/1.1\r\nHost: www.example.com\r\nUser-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\nAccept-Language: en, mi\r\n"
 			, "Host\nwww.example.com\nUser-Agent\ncurl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\nAccept-Language\nen, mi"),
-		std::make_pair("GET /search?query=openai&year=2024#conclusion HTTP/1.1\r\nHost:       www.example.com       \n"
+		std::make_pair("GET /search?query=openai&year=2024#conclusion HTTP/1.1\r\nHost:       www.example.com       \r\n"
 			, "Host\nwww.example.com"),
 	};
 	runHeaderTests("VALID HEADER", sizeof(tests) / sizeof(tests[0]), tests);
@@ -47,7 +47,7 @@ void	testValidHeader()
 void	testInvalidHeader()
 {
 	std::pair<std::string, std::string>	tests[] = {
-		std::make_pair("GET /search?query=openai&year=2024#conclusion HTTP/1.1\r\nHost :       www.example.com       \n"
+		std::make_pair("GET /search?query=openai&year=2024#conclusion HTTP/1.1\r\nHost :       www.example.com       \r\n"
 			, "Host\nwww.example.com"),
 	};
 	runHeaderTests("INVALID HEADER", sizeof(tests) / sizeof(tests[0]), tests);
