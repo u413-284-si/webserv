0f3310d04f4ac6a3af051ee7da6a4630847356d4
sirui <sirui.qiu01@gmail.com>
2024-06-22 12:49:42 +0200

    doc: add doxygen

--- src/RequestParser.cpp
+++ src/RequestParser.cpp
@@ -3,6 +3,20 @@
 
 /* ====== HELPER FUNCTIONS ====== */
 
+/**
+ * @brief Checks if a given string starts with a single space followed by a non-space character.
+ *
+ * This function checks if the provided string `str` starts with exactly one space character followed
+ * by a non-space character. If this condition is met, it returns a substring of `str` starting from 
+ * the second character. If the condition is not met, it sets the error code to 400 and throws a 
+ * `std::runtime_error` with an appropriate error message.
+ *
+ * @param str The input string to be checked.
+ * @return A substring of `str` starting from the second character if the input string starts with
+ *         a single space followed by a non-space character.
+ * @throws std::runtime_error If the input string does not start with a single space followed by a 
+ *         non-space character, an error is thrown and the error code is set to 400.
+ */
 std::string	RequestParser::checkForSpace(const std::string& str)
 {
 	if (str.length() > 1 && str[0] == ' ' && str[1] != ' ')
@@ -14,11 +28,13 @@ std::string	RequestParser::checkForSpace(const std::string& str)
 }
 
 /**
- * @brief Check for CRLF at the end of the request line.
+ * @brief Checks if a given string starts with CRLF. 
  * 
- * Only checks for the carriage return \r, as the newline \n is discarded
+ * Only checks for the carriage return \r, as the newline \\n is discarded
  * by std::getline
- * @param str	String containing remainder of request line after HTTP version
+ * @param str	Input string to be checked.
+ * @throws std::runtime_error If the input string does not contain a single
+ *         carriage return, an error is thrown and the error code is set to 400.
  */
 void	RequestParser::checkForCRLF(const std::string& str)
 {
@@ -28,6 +44,40 @@ void	RequestParser::checkForCRLF(const std::string& str)
 	}
 }
 
+/**
+ * @brief Checks if a given character is a valid URI character.
+ *
+ * This function determines if the provided character `c` is a valid character in a URI.
+ * It checks against both unreserved and reserved characters as per the URI specification.
+ *
+ * The unreserved characters include:
+ * - Alphanumeric characters (letters and digits)
+ * - Hyphen (`-`)
+ * - Period (`.`)
+ * - Underscore (`_`)
+ * - Tilde (`~`)
+ *
+ * The reserved characters include:
+ * - Colon (`:`)
+ * - Slash (`/`)
+ * - Question mark (`?`)
+ * - Hash (`#`)
+ * - Square brackets (`[`, `]`)
+ * - At symbol (`@`)
+ * - Exclamation mark (`!`)
+ * - Dollar sign (`$`)
+ * - Ampersand (`&`)
+ * - Apostrophe (`'`)
+ * - Parentheses (`(`, `)`)
+ * - Asterisk (`*`)
+ * - Plus sign (`+`)
+ * - Comma (`,`),
+ * - Semicolon (`;`)
+ * - Equals sign (`=`)
+ *
+ * @param c The character to be checked.
+ * @return `true` if the character is a valid URI character, `false` otherwise.
+ */
 bool	RequestParser::isValidURIChar(uint8_t c) const
 {
 	// Check for unreserved chars
@@ -60,6 +110,32 @@ bool	RequestParser::isValidURIChar(uint8_t c) const
     }
 }
 
+/**
+ * @brief Checks if a given character is a valid HTTP header field name character.
+ *
+ * This function determines if the provided character `c` is valid for use in an HTTP header field name.
+ * According to the HTTP/1.1 specification, valid characters for header field names include alphanumeric
+ * characters (letters and digits) and the following special characters:
+ *
+ * - Exclamation mark (`!`)
+ * - Hash (`#`)
+ * - Dollar sign (`$`)
+ * - Percent (`%`)
+ * - Ampersand (`&`)
+ * - Apostrophe (`'`)
+ * - Asterisk (`*`)
+ * - Plus sign (`+`)
+ * - Hyphen (`-`)
+ * - Period (`.`)
+ * - Caret (`^`)
+ * - Underscore (`_`)
+ * - Grave accent (`\``)
+ * - Vertical bar (`|`)
+ * - Tilde (`~`)
+ *
+ * @param c The character to be checked.
+ * @return `true` if the character is valid for an HTTP header field name, `false` otherwise.
+ */
 bool	RequestParser::isValidHeaderFieldNameChar(uint8_t c) const
 {
 	if (std::isalnum(c))
@@ -93,8 +169,52 @@ RequestParser::RequestParser() {}
 
 RequestParser::~RequestParser() {}
 
+/* ====== GETTER FUNCTIONS ====== */
+
+/**
+ * @brief Retrieves the current error code.
+ *
+ * This function returns the current error code stored in the `RequestParser` object. 
+ * The error code indicates the type of error that occurred during the parsing process.
+ *
+ * @return The current error code as an integer.
+ */
+int	RequestParser::getErrorCode() const
+{
+	return m_errorCode;
+}
+
+/**
+ * @brief Retrieves the bit flag representing the HTTP request method.
+ *
+ * This function returns the bit flag stored in the `RequestParser` object that represents the HTTP request method.
+ * The bit flag is set during the parsing process based on the method type (e.g., GET, POST, DELETE).
+ *
+ * @return The bit flag corresponding to the HTTP request method as an integer.
+ */
+int	RequestParser::getRequestMethod() const
+{
+	return m_requestMethod;
+}
+
 /* ====== MEMBER FUNCTIONS ====== */
 
+/**
+ * @brief Parses an HTTP request string into an HTTPRequest object.
+ *
+ * This function takes a raw HTTP request string and parses it into an
+ * `HTTPRequest` object. The parsing process involves several steps:
+ * 1. Parsing the request line (method, URI, and version).
+ * 2. Parsing the headers.
+ * 3. Parsing the body (if any).
+ *
+ * If any errors are encountered during parsing, an appropriate error code is
+ * set, and a `std::runtime_error` is thrown.
+ *
+ * @param request The raw HTTP request string to be parsed.
+ * @return An `HTTPRequest` object representing the parsed HTTP request.
+ * @throws std::runtime_error If there is any error during parsing.
+ */
 HTTPRequest	RequestParser::parseHttpRequest(const std::string& request)
 {
 	std::istringstream	requestStream(request);
@@ -150,6 +270,18 @@ HTTPRequest	RequestParser::parseHttpRequest(const std::string& request)
     return m_request;
 }
 
+/**
+ * @brief Parses the HTTP method from the request line.
+ *
+ * This function extracts the HTTP method from the beginning of the provided request line.
+ * It supports the "GET", "POST", and "DELETE" methods. The method is stored in `m_request.method`,
+ * and a corresponding bit flag is set in `m_requestMethod`. If the method is not recognized,
+ * an error code is set to 501 and a `std::runtime_error` is thrown.
+ *
+ * @param requestLine The request line string from which the method is to be parsed.
+ * @return A substring of `requestLine` starting from the character after the parsed method.
+ * @throws std::runtime_error If the method is not implemented, an error is thrown and the error code is set to 501.
+ */
 std::string	RequestParser::parseMethod(const std::string& requestLine)
 {
 	int	i = -1;
@@ -170,12 +302,20 @@ std::string	RequestParser::parseMethod(const std::string& requestLine)
 }
 
 /**
- * @brief Parse URI for origin server
- * 
- * used to identify a resource on an origin server or gateway.
- * 
- * @param requestLine 
- * @return std::string 
+ * @brief Parses the URI from the request line.
+ *
+ * This function extracts the URI from the provided request line.
+ * It validates the URI, ensuring it starts with a slash ('/'),
+ * and checks each character for validity.
+ * If the URI contains a query ('?') or fragment ('#'), it delegates parsing to
+ * `parseUriQuery` and `parseUriFragment` respectively.
+ * If an invalid character is encountered, an error code is set, and
+ * a `std::runtime_error` is thrown.
+ *
+ * @param requestLine The request line string from which the URI is to be parsed.
+ * @return A substring of `requestLine` starting from the character after the parsed URI.
+ * @throws std::runtime_error If the URI is missing the initial slash,
+ *         contains invalid characters, or any other URI-related errors occur.
  */
 std::string	RequestParser::parseUri(const std::string& requestLine)
 {
@@ -203,6 +343,22 @@ std::string	RequestParser::parseUri(const std::string& requestLine)
 	return (requestLine.substr(i));
 }
 
+/**
+ * @brief Parses the query component of the URI from the request line.
+ *
+ * This function extracts the query component of the URI from the provided request line.
+ * It starts parsing at the given index and continues until a space or
+ * fragment ('#') character is encountered.
+ * Each character in the query is validated using `isValidURIChar`.
+ * If an invalid character or another '?' is encountered,
+ * an error code is set, and a `std::runtime_error` is thrown.
+ *
+ * @param requestLine The request line string from which the query component of the URI is to be parsed.
+ * @param index The current index in the request line string where the query component starts.
+ *              This index is updated as the query is parsed.
+ * @throws std::runtime_error If an invalid character is encountered in the query,
+ *         or if another '?' is found, an error is thrown and the error code is set to 400.
+ */
 void	RequestParser::parseUriQuery(const std::string& requestLine, int& index)
 {
 	while (requestLine[++index]) {
@@ -219,6 +375,21 @@ void	RequestParser::parseUriQuery(const std::string& requestLine, int& index)
 	}
 }
 
+/**
+ * @brief Parses the fragment component of the URI from the request line.
+ *
+ * This function extracts the fragment component of the URI from the provided request line.
+ * It starts parsing at the given index and continues until a space character is encountered.
+ * Each character in the fragment is validated using `isValidURIChar`.
+ * If an invalid character or another '#' is encountered,
+ * an error code is set, and a `std::runtime_error` is thrown.
+ *
+ * @param requestLine The request line string from which the fragment component of the URI is to be parsed.
+ * @param index The current index in the request line string where the fragment component starts.
+ *              This index is updated as the fragment is parsed.
+ * @throws std::runtime_error If an invalid character is encountered in the fragment,
+ *         or if another '#' is found, an error is thrown and the error code is set to 400.
+ */
 void	RequestParser::parseUriFragment(const std::string& requestLine, int& index)
 {
 	while (requestLine[++index]) {
@@ -235,6 +406,20 @@ void	RequestParser::parseUriFragment(const std::string& requestLine, int& index)
 	}
 }
 
+/**
+ * @brief Parses the HTTP version from the request line.
+ *
+ * This function extracts and validates the HTTP version from the provided request line.
+ * It checks if the version format starts with "HTTP/",
+ * followed by a valid major and minor version number.
+ * If any format error is detected (e.g., missing digits or incorrect delimiter),
+ * an appropriate error code is set, and a `std::runtime_error` is thrown.
+ *
+ * @param requestLine The request line string from which the HTTP version is to be parsed.
+ * @return A substring of `requestLine` starting from the character after the parsed version.
+ * @throws std::runtime_error If the HTTP version format is invalid,
+ *         an error is thrown and the error code is set to 400.
+ */
 std::string	RequestParser::parseVersion(const std::string& requestLine)
 {
 	if (requestLine.substr(0, 5) != "HTTP/") {
@@ -260,6 +445,18 @@ std::string	RequestParser::parseVersion(const std::string& requestLine)
 	return (requestLine.substr(++i));
 }
 
+/**
+ * @brief Checks the validity of an HTTP header field name.
+ *
+ * This function verifies if the provided HTTP header field name is valid
+ * according to HTTP specifications.
+ * It ensures that the header name does not end with whitespace and that all
+ * characters in the header name are valid according to `isValidHeaderFieldNameChar`.
+ *
+ * @param headerName The HTTP header field name to be validated.
+ * @throws std::runtime_error If the header name contains invalid characters or
+ *         ends with whitespace, an error is thrown and the error code is set to 400.
+ */
 void	RequestParser::checkHeaderName(const std::string& headerName)
 {
 	if (isspace(headerName[headerName.size() - 1])){
@@ -274,6 +471,22 @@ void	RequestParser::checkHeaderName(const std::string& headerName)
 	}
 }
 
+/**
+ * @brief Checks the validity of the Content-Length header field.
+ *
+ * This function verifies the validity of the Content-Length header field in an HTTP request.
+ * It ensures that:
+ * - There is only one Content-Length header or multiple with the same value.
+ * - The value of Content-Length is a valid numeric string.
+ * If the header is invalid or there are multiple Content-Length headers with conflicting values,
+ * an appropriate error code is set, and a `std::runtime_error` is thrown.
+ *
+ * @param headerName The name of the HTTP header field.
+ * @param headerValue The value associated with the HTTP header field.
+ * @throws std::runtime_error If there are multiple conflicting Content-Length
+ *         headers or if the Content-Length value is invalid, an error is thrown
+ *         and the error code is set to 400.
+ */
 void	RequestParser::checkContentLength(const std::string& headerName, const std::string& headerValue)
 {
 	if (headerName == "Content-Length") {
@@ -293,6 +506,19 @@ void	RequestParser::checkContentLength(const std::string& headerName, const std:
 	}
 }
 
+/**
+ * @brief Checks the validity of Transfer-Encoding header field.
+ *
+ * This function verifies the validity of the Transfer-Encoding header field in an HTTP request.
+ * It ensures that:
+ * - If the Transfer-Encoding header exists, it is not empty.
+ * - The last encoding in the Transfer-Encoding list is "chunked".
+ * If the header is invalid or the encoding is incorrect, an appropriate error code is set,
+ * and a `std::runtime_error` is thrown.
+ *
+ * @throws std::runtime_error If the Transfer-Encoding header is missing or empty,
+ *         or if the last encoding is not "chunked", an error is thrown and the error code is set to 400.
+ */
 void	RequestParser::checkTransferEncoding()
 {
 	if (m_request.headers.find("Transfer-Encoding") != m_request.headers.end()) {
