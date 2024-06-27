8312fce21a6eb767db8842d460fa980443dc7495
Gabriel Wolf <gwolf@student.42vienna.com>
2024-06-27 11:52:40 +0000

    feat: add init functions for logger

---
+++ src/LogInit.cpp
@@ -0,0 +1,13 @@
+#include "Log.hpp"
+
+Logger& initFile(LogLevel level, const char * filename)
+{
+	static LogOutputterFile outputter(filename);
+	return Logger::init(level, &outputter);
+}
+
+Logger& initConsole(LogLevel level)
+{
+	static LogOutputterConsole outputter;
+	return Logger::init(level, &outputter);
+}
