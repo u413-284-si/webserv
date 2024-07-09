697a136225ca0bbb3b4b4569245dd30b73e38bb7
sirui <sirui.qiu01@gmail.com>
2024-07-10 00:20:25 +0200

    refactor: move HTTP request struct to separate header

---
+++ inc/HTTPRequest.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+/* ====== LIBRARIES ====== */
+
+#include <iostream>
+#include <map>
+
+/* ====== DEFINITIONS ====== */
+
+struct URI {
+	std::string		path;
+	std::string		query;
+	std::string		fragment;
+};
+
+struct HTTPRequest {
+	std::string							method;
+	URI									uri;
+	std::string							version;
+	std::map<std::string, std::string>	headers;
+	std::string							body;
+	bool								hasBody;
+	bool								chunked;
+};
--- inc/RequestParser.hpp
+++ inc/RequestParser.hpp
@@ -3,6 +3,7 @@
 /* ====== LIBRARIES ====== */
 
 #include "utilities.hpp"
+#include "HTTPRequest.hpp"
 #include <iostream>
 #include <stdexcept>
 #include <map>
@@ -11,22 +12,6 @@
 #include <vector>
 
 /* ====== DEFINITIONS ====== */
-
-struct URI {
-	std::string		path;
-	std::string		query;
-	std::string		fragment;
-};
-
-struct HTTPRequest {
-	std::string							method;
-	URI									uri;
-	std::string							version;
-	std::map<std::string, std::string>	headers;
-	std::string							body;
-	bool								hasBody;
-	bool								chunked;
-};
 
 /* ====== CLASS DECLARATION ====== */
 
