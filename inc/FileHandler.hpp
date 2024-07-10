8e27c02e2b48b5c8be20030aa7a9561216294451
Gabriel Wolf <gwolf@student.42vienna.com>
2024-07-10 07:29:51 +0000

    fix: make checkFileType and enum public

--- inc/FileHandler.hpp
+++ inc/FileHandler.hpp
@@ -11,11 +11,11 @@
 class FileHandler {
 
 public:
+	virtual ~FileHandler();
+
+	enum fileType { StatError = -1, FileNotExist = 0, FileDirectory = 1, FileRegular = 2, FileOther = 3 };
+	virtual fileType checkFileType(const std::string& path) const;
 	virtual bool isDirectory(const std::string& path) const;
 	virtual bool isExistingFile(const std::string& path) const;
 	virtual std::string getFileContents(const char* filename) const;
-
-private:
-	enum fileType { NotExist = 0, Directory = 1, RegularFile = 2, Other = 3 };
-	fileType checkFileType(const std::string& path) const;
 };
