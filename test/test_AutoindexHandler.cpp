3d1d16ff563b7642fe59db696070261eb90d6810
Gabriel Wolf <gwolf@student.42vienna.com>
2024-07-12 11:13:03 +0000

    fix: add test to check for throwing in Directory

--- test/test_AutoindexHandler.cpp
+++ test/test_AutoindexHandler.cpp
@@ -1,14 +1,26 @@
+#include "gmock/gmock.h"
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <stdexcept>
 
 #include "MockFileSystemPolicy.hpp"
 
 #include "AutoindexHandler.hpp"
 
-TEST(AutoindexHandler, Autoindex)
+TEST(AutoindexHandler, DirectoryThrow)
 {
 	MockFileSystemPolicy fileSystemPolicy;
 	AutoindexHandler autoindexHandler(fileSystemPolicy);
+
+	DIR* dir = opendir("/proc/self/");
+
+	EXPECT_CALL(fileSystemPolicy, openDirectory)
+	.WillOnce(testing::Return(dir));
+	EXPECT_CALL(fileSystemPolicy, readDirectory)
+	.WillOnce(testing::Throw(std::runtime_error("readDirectory failed")));
+	EXPECT_CALL(fileSystemPolicy, closeDirectory)
+	.WillOnce(testing::Return(0));
+
 	std::string autoindex = autoindexHandler.execute("/workspaces/webserv/test/");
-	EXPECT_EQ(autoindex, "..");
+	EXPECT_EQ(autoindex, "");
 }
