823e6fd0dd52a5c2a98d4b61c673cea13854a378
Gabriel Wolf <gwolf@student.42vienna.com>
2024-07-14 16:07:22 +0000

    doxygen

--- src/FileSystemPolicy.cpp
+++ src/FileSystemPolicy.cpp
@@ -1,7 +1,23 @@
 #include "FileSystemPolicy.hpp"
 
+/**
+ * @brief Virtual destructor for FileSystemPolicy object
+ *
+ */
 FileSystemPolicy::~FileSystemPolicy() {}
 
+/**
+ * @brief Check the file type of a given path.
+ *
+ * Uses stat() to check the file type of a given path.
+ * If the file does not exist, FileNotExist is returned.
+ * If the file is a regular file, FileRegular is returned.
+ * If the file is a directory, FileDirectory is returned.
+ * If the file is neither a regular file nor a directory, FileOther is returned.
+ * @throws std::runtime_error with strerror() of errno.
+ * @param path Path to check.
+ * @return FileSystemPolicy::fileType File type.
+ */
 FileSystemPolicy::fileType FileSystemPolicy::checkFileType(const std::string& path) const
 {
 	struct stat fileStat = {};
@@ -18,10 +34,31 @@ FileSystemPolicy::fileType FileSystemPolicy::checkFileType(const std::string& pa
 	return FileOther;
 }
 
+/**
+ * @brief Wrapper to check if a path is a directory.
+ *
+ * @param path Path to check.
+ * @return true Path is a directory.
+ * @return false Path is not a directory.
+ */
 bool FileSystemPolicy::isDirectory(const std::string& path) const { return checkFileType(path) == FileDirectory; }
 
+/**
+ * @brief Wrapper to check if a path is an existing file.
+ *
+ * @param path Path to check.
+ * @return true Path is an existing file.
+ * @return false Path is not an existing file.
+ */
 bool FileSystemPolicy::isExistingFile(const std::string& path) const { return checkFileType(path) != FileNotExist; }
 
+/**
+ * @brief Gets the contents of a file.
+ *
+ * @throws std::runtime_error with strerror() of errno.
+ * @param filename File to read.
+ * @return std::string File contents.
+ */
 std::string FileSystemPolicy::getFileContents(const char* filename) const
 {
 	errno = 0;
@@ -37,6 +74,13 @@ std::string FileSystemPolicy::getFileContents(const char* filename) const
 	return contents;
 }
 
+/**
+ * @brief Opens a directory.
+ *
+ * @throws std::runtime_error with strerror() of errno.
+ * @param path Path to open.
+ * @return DIR* Directory stream.
+ */
 DIR* FileSystemPolicy::openDirectory(const std::string& path) const
 {
 	errno = 0;
@@ -47,6 +91,13 @@ DIR* FileSystemPolicy::openDirectory(const std::string& path) const
 	return dir;
 }
 
+/**
+ * @brief Reads a directory.
+ *
+ * @throws std::runtime_error with strerror() of errno.
+ * @param dir Directory stream.
+ * @return struct dirent* Directory entry.
+ */
 struct dirent* FileSystemPolicy::readDirectory(DIR* dir) const
 {
 	errno = 0;
@@ -57,6 +108,12 @@ struct dirent* FileSystemPolicy::readDirectory(DIR* dir) const
 	return entry;
 }
 
+/**
+ * @brief Closes a directory.
+ *
+ * @param dir Directory stream.
+ * @return int errno Error code.
+ */
 int FileSystemPolicy::closeDirectory(DIR* dir) const
 {
 	errno = 0;
@@ -66,6 +123,13 @@ int FileSystemPolicy::closeDirectory(DIR* dir) const
 	return 0;
 }
 
+/**
+ * @brief Gets the file status of a path.
+ *
+ * @throws std::runtime_error with strerror() of errno.
+ * @param path Path to check.
+ * @return struct stat File status.
+ */
 struct stat FileSystemPolicy::getFileStat(const std::string& path) const
 {
 	struct stat fileStat = {};
