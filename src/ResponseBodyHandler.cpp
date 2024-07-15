1418bfc0b4d9b6c8f8efc201b80670241c8a49e3
Gabriel Wolf <gwolf@student.42vienna.com>
2024-07-15 14:25:25 +0000

    fix: set content type of default error page

--- src/ResponseBodyHandler.cpp
+++ src/ResponseBodyHandler.cpp
@@ -60,4 +60,6 @@ void ResponseBodyHandler::execute()
 void ResponseBodyHandler::handleErrorBody()
 {
 	m_response.body = utils::getDefaultErrorPage(m_response.status);
+	// This is just to get the right extension. Could be put into its own data field of HTML struct.
+	m_response.targetResource = "error.html";
 }
