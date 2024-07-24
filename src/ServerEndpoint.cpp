58be631798d1645da8f64a6b201c71164a18dc05
Gabriel Wolf <gwolf@student.42vienna.com>
2024-07-24 10:28:23 +0000

    feat: handle_event for server

--- src/ServerEndpoint.cpp
+++ src/ServerEndpoint.cpp
@@ -1,4 +1,5 @@
 #include "ServerEndpoint.hpp"
+#include "ClientEndpoint.hpp"
 #include "Dispatcher.hpp"
 
 ServerEndpoint::ServerEndpoint(const Connection& connection)
@@ -10,25 +11,34 @@ ServerEndpoint::~ServerEndpoint() { close(m_connection.fd); }
 
 void ServerEndpoint::handleEvent(Dispatcher& dispatcher)
 {
-	struct sockaddr_in clientAddr;
-	socklen_t addr_len = sizeof(clientAddr);
-	int clientSock = accept(m_connection.fd, (struct sockaddr*)&clientAddr, &addr_len);
+	struct sockaddr_storage clientAddr = { };
+	socklen_t clientLen = sizeof(clientAddr);
+	const int clientSock = accept(m_connection.fd, reinterpret_cast<struct sockaddr*>(&clientAddr), &clientLen);
 	if (clientSock < 0) {
 		if (errno == EAGAIN || errno == EWOULDBLOCK) {
 			return; // No more pending connections
-		} else {
-			LOG_ERROR << "error: accept: " << strerror(errno) << '\n';
-			return;
 		}
+		LOG_ERROR << "accept: " << strerror(errno) << '\n';
+		return;
 	}
 
+	char bufHost[NI_MAXHOST];
+	char bufPort[NI_MAXSERV];
+	getnameinfo(reinterpret_cast<struct sockaddr*>(&clientAddr), clientLen, bufHost, NI_MAXHOST, bufPort, NI_MAXSERV,
+		NI_NUMERICHOST | NI_NUMERICSERV);
+	const Connection connection = { clientSock, bufHost, bufPort };
+	IEndpoint* endpoint = new ClientEndpoint(connection, m_connection);
+
 	// Add client socket to epoll instance
-	struct epoll_event ev;
-	ev.events = EPOLLIN;
-	ev.data.fd = clientSock;
-	if (!dispatcher.addEvent(clientSock, &ev)) {
+	struct epoll_event event = { };
+	event.events = EPOLLIN | EPOLLOUT;
+	event.data.ptr = static_cast<void*>(endpoint);
+
+	if (!dispatcher.addEvent(clientSock, &event, endpoint)) {
 		close(clientSock);
+		delete endpoint;
 	}
+	LOG_INFO << "Added client endpoint: " << connection.host << ':' << connection.port;
 }
 
 time_t ServerEndpoint::getTimeSinceLastEvent() const { return (0); }
