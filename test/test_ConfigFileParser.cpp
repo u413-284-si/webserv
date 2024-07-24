3016d8d0a0041efd0bd1c0991d8104ec5c7f4b7d
Chris <94694991+QCHR1581@users.noreply.github.com>
2024-07-24 14:23:44 +0000

    style: formatted files

--- test/test_ConfigFileParser.cpp
+++ test/test_ConfigFileParser.cpp
@@ -1,22 +1,20 @@
+#include "ConfigFileParser.hpp"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-#include "ConfigFileParser.hpp"
 
-class InvalidConfigFileTests : public ::testing::Test
-{
-	protected:
-		ConfigFileParser m_configFileParser;
+class InvalidConfigFileTests : public ::testing::Test {
+protected:
+    ConfigFileParser m_configFileParser;
 };
 
-class ValidConfigFileTests : public ::testing::Test
-{
-	protected:
-		ConfigFileParser m_configFileParser;
+class ValidConfigFileTests : public ::testing::Test {
+protected:
+    ConfigFileParser m_configFileParser;
 };
 
 /**
  * @brief Tests for an invalid file
- * 
+ *
  * Checks if the following configuration are seen as invalid:
  * 1. File could not be opened (because the path is wrong, such a file does not exist etc.)
  * 2. File is empty
@@ -31,251 +29,219 @@ class ValidConfigFileTests : public ::testing::Test
  * 11. Root directive has multiple root paths
  */
 
-
 TEST_F(InvalidConfigFileTests, FileCouldNotBeOpened)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("invalid_file");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Failed to open config file", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("invalid_file");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Failed to open config file", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, FileIsEmpty)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/empty_file.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Config file is empty", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/empty_file.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Config file is empty", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, FileContainsMissingBracket)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/missing_bracket.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Open bracket(s) in config file", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/missing_bracket.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Open bracket(s) in config file", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, FileContainsTooManyBrackets)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/too_many_brackets.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Open bracket(s) in config file", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/too_many_brackets.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Open bracket(s) in config file", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, FileMissesHtppBlock)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/missing_http_block.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Config file does not start with 'http {'", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/missing_http_block.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Config file does not start with 'http {'", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, FileMissesServerBlock)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/missing_server_block.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("No server(s) in config file", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/missing_server_block.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("No server(s) in config file", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, FileContainsInvalidServerDirective)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/invalid_server_directive.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Invalid server directive", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/invalid_server_directive.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Invalid server directive", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, FileContainsInvalidLocationDirective)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/invalid_location_directive.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Invalid location directive", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/invalid_location_directive.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Invalid location directive", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, FileContainsMissingSemicolon)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/missing_semicolon.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Semicolon missing", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/missing_semicolon.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Semicolon missing", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, ListenDirectiveContainsTooHighIpAddress)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/listen_invalid_ip_high.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Invalid ip address", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/listen_invalid_ip_high.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Invalid ip address", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, ListenDirectiveContainsIpAddressWithMissingDot)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/listen_invalid_ip_missing_dot.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Invalid ip address", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/listen_invalid_ip_missing_dot.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Invalid ip address", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, ListenDirectiveContainsTooHighPort)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/listen_invalid_port_high.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Invalid port", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/listen_invalid_port_high.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Invalid port", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, ListenDirectiveContainsTooLowPort)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/listen_invalid_port_low.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("Invalid port", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/listen_invalid_port_low.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Invalid port", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, RootDirectiveContainsNoPath)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/root_no_path.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("'root' directive has no value", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/root_no_path.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("'root' directive has no value", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 TEST_F(InvalidConfigFileTests, RootDirectiveContainsMultipleRootPaths)
 {
-	EXPECT_THROW(
-	{
-		try
-		{
-			m_configFileParser.parseConfigFile("config_files/root_multiple_paths.conf");
-		}
-		catch (const std::exception& e)
-		{
-			EXPECT_STREQ("More than one root path", e.what());
-			throw;
-		}
-	}, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                m_configFileParser.parseConfigFile("config_files/root_multiple_paths.conf");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("More than one root path", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
-
 
 /**
  * @brief Tests for a valid file
- * 
+ *
  * Checks if the following configuration are seen as valid:
  * 1. A standard valid file
  * 2. Listen directive contains only ip
@@ -286,19 +252,19 @@ TEST_F(InvalidConfigFileTests, RootDirectiveContainsMultipleRootPaths)
 TEST_F(ValidConfigFileTests, ValidFile)
 
 {
-	EXPECT_NO_THROW([&]() {m_configFileParser.parseConfigFile("config_files/valid_config.conf");});
+    EXPECT_NO_THROW([&]() { m_configFileParser.parseConfigFile("config_files/valid_config.conf"); });
 }
 TEST_F(ValidConfigFileTests, ListenContainsOnlyIp)
 {
-	EXPECT_NO_THROW([&]() {m_configFileParser.parseConfigFile("config_files/listen_only_ip.conf");});
+    EXPECT_NO_THROW([&]() { m_configFileParser.parseConfigFile("config_files/listen_only_ip.conf"); });
 }
 
 TEST_F(ValidConfigFileTests, ListenContainsOnlyPort)
 {
-	EXPECT_NO_THROW([&]() {m_configFileParser.parseConfigFile("config_files/listen_only_port.conf");});
+    EXPECT_NO_THROW([&]() { m_configFileParser.parseConfigFile("config_files/listen_only_port.conf"); });
 }
 
 TEST_F(ValidConfigFileTests, ListenContainsIpAndPort)
 {
-	EXPECT_NO_THROW([&]() {m_configFileParser.parseConfigFile("config_files/listen_ip_and_port.conf");});
+    EXPECT_NO_THROW([&]() { m_configFileParser.parseConfigFile("config_files/listen_ip_and_port.conf"); });
 }
