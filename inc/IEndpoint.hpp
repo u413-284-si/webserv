2cd8de3bb5e22ffde02f3d2edd91e54b00363157
Gabriel Wolf <gwolf@student.42vienna.com>
2024-07-24 13:14:07 +0000

    fix: add eventMask to IEndpoint

--- inc/IEndpoint.hpp
+++ inc/IEndpoint.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <ctime>
+#include <stdint.h>
 
 class Dispatcher;
 
 class IEndpoint {
 public:
 	virtual ~IEndpoint() { }
-	virtual void handleEvent(Dispatcher& dispatcher) = 0;
+	virtual void handleEvent(Dispatcher& dispatcher, uint32_t eventMask) = 0;
 	virtual time_t getTimeSinceLastEvent() const = 0;
 };
