15a2b1931dfa920545ed6b122c10e89b79ce45e0
sirui <sirui.qiu01@gmail.com>
2024-07-27 10:46:41 +0000

    format: apply Webkit style

--- inc/StatusCode.hpp
+++ inc/StatusCode.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 enum statusCode {
-		StatusOK = 200,
-		StatusMovedPermanently = 301,
-		StatusBadRequest = 400,
-		StatusForbidden = 403,
-		StatusNotFound = 404,
-		StatusMethodNotAllowed = 405,
-		StatusInternalServerError = 500,
-        StatusMethodNotImplemented = 501,
-        StatusNonSupportedVersion = 505
+	StatusOK = 200,
+	StatusMovedPermanently = 301,
+	StatusBadRequest = 400,
+	StatusForbidden = 403,
+	StatusNotFound = 404,
+	StatusMethodNotAllowed = 405,
+	StatusInternalServerError = 500,
+	StatusMethodNotImplemented = 501,
+	StatusNonSupportedVersion = 505
 };
