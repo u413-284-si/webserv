2eeb067f8f90891e21e987cf6fd14f40d998bf7c
Gabriel Wolf <gwolf@student.42vienna.com>
2024-08-02 13:32:52 +0000

    feat: add  receivedBytes

--- inc/Connection.hpp
+++ inc/Connection.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Socket.hpp"
+#include <cstddef>
 #include <ctime>
 #include <unistd.h>
 
@@ -12,15 +13,18 @@ public:
 
 	void setStatus(ConnectionStatus status);
 	void closeConnection() const;
+	void updateBytesReceived(std::size_t bytesReceived);
 
 	Socket getServer() const;
 	Socket getClient() const;
 	time_t getTimeSinceLastEvent() const;
 	ConnectionStatus getStatus() const;
+	std::size_t getBytesReceived() const;
 
 private:
 	Socket m_server;
 	Socket m_client;
 	time_t m_TimeSinceLastEvent;
 	ConnectionStatus m_status;
+	std::size_t m_bytesReceived;
 };
--- src/Connection.cpp
+++ src/Connection.cpp
@@ -5,12 +5,15 @@ Connection::Connection(const Socket& server, const Socket& client)
 	, m_client(client)
 	, m_TimeSinceLastEvent(std::time(0))
 	, m_status(ReceiveRequest)
+	, m_bytesReceived(0)
 {
 }
 
 void Connection::closeConnection() const { close(m_client.fd); }
 
 void Connection::setStatus(ConnectionStatus status) { m_status = status; }
+
+void Connection::updateBytesReceived(std::size_t bytesReceived) { m_bytesReceived += bytesReceived; }
 
 Socket Connection::getServer() const { return m_server; }
 
@@ -19,3 +22,5 @@ Socket Connection::getClient() const { return m_client; }
 time_t Connection::getTimeSinceLastEvent() const { return (std::time(0) - m_TimeSinceLastEvent); }
 
 Connection::ConnectionStatus Connection::getStatus() const { return m_status; }
+
+std::size_t Connection::getBytesReceived() const { return m_bytesReceived; }
