96c599ebdbe1efe4c377cc936ee0292969b9de13
Gabriel Wolf <gwolf@student.42vienna.com>
2024-08-02 09:54:16 +0000

    feat: integrate alternative design to dynamic dispatch

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -3,9 +3,12 @@
 /* ====== LIBRARIES ====== */
 
 #include "ConfigFile.hpp"
+#include "Connection.hpp"
+#include "FileSystemPolicy.hpp"
 #include "Log.hpp"
 #include "RequestParser.hpp"
 #include "ResponseBuilder.hpp"
+#include "Socket.hpp"
 #include "StatusCode.hpp"
 
 #include <algorithm>
@@ -15,11 +18,14 @@
 #include <cstring>
 #include <iostream>
 #include <map>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <stdexcept>
 #include <sys/epoll.h>
 #include <sys/socket.h>
+#include <string>
 #include <unistd.h>
+#include <utility>
 
 /* ====== DEFINITIONS ====== */
 
@@ -32,20 +38,53 @@
 
 class Server {
 private:
-	int m_serverSock;
-	int m_epfd;
-	std::map<int, std::string> m_requestStrings;
-
-	void acceptConnection() const;
-	void handleConnections(int clientSock, RequestParser& parser);
-	bool checkForCompleteRequest(int clientSock);
-
-	Server(const Server& ref);
-	Server& operator=(const Server& ref);
+	static const int s_epollTimeout = 1000;
+	static const int s_epollMaxEvents = 10;
+	static const int s_backlog = 10;
+	static const time_t s_clientTimeout = 60;
 
 public:
-	Server();
+	explicit Server(
+		const ConfigFile& configFile, int epollTimeout = s_epollTimeout, size_t maxEvents = s_epollMaxEvents);
 	~Server();
 
 	void run();
+
+private:
+	const ConfigFile& m_configFile; /**< Global config file */
+	int m_epfd; /**< FD of epoll instance */
+	int m_epollTimeout; /**< Timeout for epoll instance */
+	std::vector<struct epoll_event> m_epollEvents; /**< Holds epoll events */
+	int m_backlog; /**< Backlog for listening sockets */
+	time_t m_clientTimeout; /**< Timeout for a Connection */
+	std::map<int, Socket> m_virtualServers; /**< Listening sockets of virtual servers */
+	std::map<int, Connection> m_connections; /**< Current active Connections */
+	std::map<int, std::string> m_requestStrings; /**< Buffers for active Connections */
+	RequestParser m_requestParser; /**< Handles parsing of request */
+	FileSystemPolicy m_fileSystemPolicy; /**< Handles functions for file system manipulation */
+	ResponseBuilder m_responseBuilder; /**< Handles building of response */
+
+	bool init();
+	bool addVirtualServer(const std::string& host, int backlog, const std::string& port);
+
+	void acceptConnections(const Socket& serverSock, uint32_t eventMask);
+	void handleConnections(const Connection& connection);
+	void handleTimeout();
+
+	bool checkDuplicateServer(const std::string& host, const std::string& port);
+	bool checkForCompleteRequest(int clientSock);
+	bool registerVirtualServer(const Socket& serverSock);
+	bool registerConnection(const Socket& serverSock, const Socket& clientSock);
+
+
+
+	Server(const Server& ref);
+	Server& operator=(const Server& ref);
 };
+
+int createListeningSocket(struct addrinfo* addrinfo, int backlog);
+Socket retrieveSocketInfo(int sockFd, const struct sockaddr* sockaddr, socklen_t socklen);
+
+bool addEvent(int epfd, int newfd, epoll_event* event);
+void removeEvent(int epfd, int delfd);
+bool modifyEvent(int epfd, int modfd, epoll_event* event);
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,61 +1,31 @@
 #include "Server.hpp"
-#include "ConfigFile.hpp"
 
 /* ====== CONSTRUCTOR/DESTRUCTOR ====== */
 
 /**
  * @brief Constructor for the Server class.
  *
- * This constructor initializes a server instance by creating a server socket,
- * using TCP protocol SOCK_STREAM, and setting it to non-blocking mode.
- * It also creates an epoll instance for event handling.
+ * This constructor initializes an epoll instance for event handling.
+ * Then it calls Server::init() to initialize virtual servers, as they are
+ * defined in the configuration file.
  * If any of these operations fail, the constructor throws a std::runtime_error.
- * Then it bind() the server socket to a specific address and port, listen() on the server socket
- * Then the server socket is added to the epoll instance.
  *
- * @throws std::runtime_error if any of the socket creation, binding, listening,
- *         epoll creation, or epoll control operations fail.
+ * @throws std::runtime_error if epoll_create or Server::init() fail.
  *
  */
-Server::Server()
-	: m_serverSock(socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0))
+Server::Server(const ConfigFile& configFile, int epollTimeout, size_t maxEvents)
+	: m_configFile(configFile)
 	, m_epfd(epoll_create(1))
+	, m_epollTimeout(epollTimeout)
+	, m_epollEvents(maxEvents)
+	, m_backlog(s_backlog)
+	, m_clientTimeout(s_clientTimeout)
+	, m_responseBuilder(m_configFile, m_fileSystemPolicy)
 {
-	if (m_serverSock < 0)
-		throw std::runtime_error("socket");
-
-	if (m_epfd < 0) {
-		close(m_serverSock);
-		throw std::runtime_error("epoll_create");
-	}
-
-	// Bind server socket
-	struct sockaddr_in serverAddr = {};
-	serverAddr.sin_family = AF_INET; // Communicates with IPv4
-	serverAddr.sin_addr.s_addr = INADDR_ANY; // Accept any arriving ip addresses
-	serverAddr.sin_port = htons(PORT); // Listen on specified port
-
-	// NOLINTNEXTLINE: Ignore reinterpret_cast warning, as it is necessary for the bind function
-	if (bind(m_serverSock, reinterpret_cast<struct sockaddr*>(&serverAddr), sizeof(serverAddr)) < 0) {
-		close(m_serverSock);
-		throw std::runtime_error("bind");
-	}
-
-	// Listen on server socket
-	if (listen(m_serverSock, CONNECTION_QUEUE) < 0) {
-		close(m_serverSock);
-		throw std::runtime_error("listen");
-	}
-
-	// Add server socket to epoll instance
-	struct epoll_event event = {};
-	event.events = EPOLLIN | EPOLLET; // Edge-triggered mode
-	event.data.fd = m_serverSock;
-	if (epoll_ctl(m_epfd, EPOLL_CTL_ADD, m_serverSock, &event) < 0) {
-		close(m_serverSock);
-		close(m_epfd);
-		throw std::runtime_error("epoll_ctl: m_serverSock");
-	}
+	if (m_epfd < 0)
+		throw std::runtime_error("epoll_create:" + std::string(strerror(errno)));
+	if (!init())
+		throw std::runtime_error("Failed to initialize virtual servers");
 }
 
 /**
@@ -71,41 +41,7 @@ Server::Server()
  * 2. Closes the epoll instance to release associated resources and stop
  * monitoring events.
  */
-Server::~Server()
-{
-	close(m_serverSock);
-	close(m_epfd);
-}
-
-/**
- * @brief Copy constructor for the Server class.
- *
- * Private, should not be used.
- * @param ref Server instance to copy.
- */
-Server::Server(const Server& ref)
-	: m_serverSock(ref.m_serverSock)
-	, m_epfd(ref.m_epfd)
-{
-}
-
-/**
- * @brief Assignment operator for the Server class.
- *
- * Private, should not be used.
- * @param ref Server instance to copy.
- * @return Server& Reference to the current instance.
- */
-Server& Server::operator=(const Server& ref)
-{
-	if (this == &ref)
-		return *this;
-
-	m_serverSock = ref.m_serverSock;
-	m_epfd = ref.m_epfd;
-
-	return *this;
-}
+Server::~Server() { close(m_epfd); }
 
 /* ====== MEMBER FUNCTIONS ====== */
 
@@ -121,22 +57,156 @@ Server& Server::operator=(const Server& ref)
  */
 void Server::run()
 {
-	RequestParser parser;
+	LOG_INFO << "Server started";
 
 	while (true) {
-		struct epoll_event events[MAX_EVENTS];
-		// Blocking call to epoll_wait
-		int nfds = epoll_wait(m_epfd, events, MAX_EVENTS, -1);
-		if (nfds < 0)
-			throw std::runtime_error("epoll_wait");
+		const int nfds = epoll_wait(m_epfd, &m_epollEvents[0], static_cast<int>(m_epollEvents.size()), m_epollTimeout);
+		if (nfds == -1)
+			throw std::runtime_error("epoll_wait:" + std::string(strerror(errno)));
+		if (nfds == 0)
+			LOG_DEBUG << "epoll_wait: Timeout";
+		else
+			LOG_DEBUG << "epoll_wait: " << nfds << " events";
 
-		for (int i = 0; i < nfds; ++i) {
-			if (events[i].data.fd == m_serverSock)
-				acceptConnection();
+		for (std::vector<struct epoll_event>::const_iterator iterEpoll = m_epollEvents.begin();
+			 iterEpoll != m_epollEvents.begin() + nfds; ++iterEpoll) {
+			uint32_t eventMask = iterEpoll->events;
+			if ((eventMask & EPOLLERR) != 0) {
+				LOG_DEBUG << "epoll_wait: EPOLLERR";
+				eventMask = EPOLLOUT;
+			} else if ((eventMask & EPOLLHUP) != 0) {
+				LOG_DEBUG << "epoll_wait: EPOLLHUP";
+				eventMask = EPOLLOUT;
+			}
+			std::map<int, Socket>::const_iterator iterSock = m_virtualServers.find(iterEpoll->data.fd);
+			if (iterSock != m_virtualServers.end())
+				acceptConnections(iterSock->second, eventMask);
 			else
-				handleConnections(events[i].data.fd, parser);
+				handleConnections(m_connections.at(iterEpoll->data.fd));
+		}
+		// handleTimeout();
+	}
+}
+
+void Server::handleTimeout()
+{
+	for (std::map<int, Connection>::iterator iter = m_connections.begin(); iter != m_connections.end(); ++iter) {
+		if (iter->second.getTimeSinceLastEvent() > m_clientTimeout) {
+			LOG_INFO << "Connection timeout: ";
+			removeEvent(m_epfd, iter->first);
+			m_connections.erase(iter);
 		}
 	}
+}
+
+bool Server::checkDuplicateServer(const std::string& host, const std::string& port)
+{
+	for (std::map<int, Socket>::const_iterator iter = m_virtualServers.begin();
+		 iter != m_virtualServers.end(); ++iter) {
+		if (iter->second.host == host && iter->second.port == port) {
+			LOG_DEBUG << "Virtual server already exists: " << iter->second;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool Server::init()
+{
+	LOG_INFO << "Initializing virtual servers";
+
+	for (std::vector<ServerConfig>::const_iterator iter = m_configFile.serverConfigs.begin();
+		 iter != m_configFile.serverConfigs.end(); ++iter) {
+
+		LOG_DEBUG << "Adding virtual server: " << iter->serverName << " on " << iter->host << ":" << iter->port;
+
+		if (checkDuplicateServer(iter->host, webutils::toString(iter->port)))
+			continue;
+
+		if (!addVirtualServer(iter->host, m_backlog, webutils::toString(iter->port))) {
+			LOG_DEBUG << "Failed to add virtual server: " << iter->serverName;
+			continue;
+		}
+	}
+
+	if (m_virtualServers.empty()) {
+		LOG_ERROR << "No virtual servers added";
+		return false;
+	}
+
+	LOG_INFO << "Finished setting up virtual servers";
+	return true;
+}
+
+bool Server::addVirtualServer(const std::string& host, const int backlog, const std::string& port)
+{
+	const char* node = NULL;
+
+	if (!host.empty() || host != "*")
+		node = host.c_str();
+
+	struct addrinfo hints = {
+		AI_PASSIVE, /* .ai_flags - If node is NULL returned address will be suitable to bind(2) a socket which can accept(2) connections.*/
+		AF_UNSPEC, /*.ai_family - Allow IPv4 or IPv6 */
+		SOCK_STREAM, /* .ai_socktype - TCP uses SOCK_STREAM */
+		0, /* .ai_protocol - Accept any protocoll */
+		sizeof(struct addrinfo), /* .ai_addrlen - not used */
+		NULL, /* .ai_addr - not used */
+		NULL, /* .ai_canonname - not used */
+		NULL /* .ai_next - not used */
+	};
+
+	struct addrinfo* list = NULL;
+	const int result = getaddrinfo(node, port.c_str(), &hints, &list);
+	if (result != 0) {
+		LOG_ERROR << "getaddrinfo(): " << gai_strerror(result);
+		return false;
+	}
+
+	size_t successfulSock = 0;
+	size_t countTryCreateSocket = 1;
+	for (struct addrinfo* curr = list; curr != NULL; curr = curr->ai_next) {
+		LOG_DEBUG << countTryCreateSocket << ". try to create listening socket";
+
+		const int newFd = createListeningSocket(curr, backlog);
+		if (newFd == -1)
+			continue;
+
+		const Socket serverSock = retrieveSocketInfo(newFd, curr->ai_addr, curr->ai_addrlen);
+		if (serverSock.fd == -1)
+			continue;
+
+		if (!registerVirtualServer(serverSock))
+			continue;
+
+		++successfulSock;
+	}
+	freeaddrinfo(list);
+
+	if (successfulSock == 0) {
+		LOG_ERROR << "Cannot bind to a valid socket.";
+		return false;
+	}
+
+	return true;
+}
+
+bool Server::registerVirtualServer(const Socket& serverSock)
+{
+	struct epoll_event event = {};
+	event.events = EPOLLIN;
+	event.data.fd = serverSock.fd;
+
+	if (!addEvent(m_epfd, serverSock.fd, &event)) {
+		close(serverSock.fd);
+		LOG_ERROR << "Failed to add event for " << serverSock;
+		return false;
+	}
+
+	m_virtualServers[serverSock.fd] = serverSock;
+
+	LOG_INFO << "New virtual server: " << serverSock;
+	return true;
 }
 
 /**
@@ -152,31 +222,55 @@ void Server::run()
  * EWOULDBLOCK (equivalent error codes indicating that a non-blocking operation
  * would normally block), meaning that no more connections are pending.
  */
-void Server::acceptConnection() const
+void Server::acceptConnections(const Socket& serverSock, uint32_t eventMask)
 {
-	while (true) {
-		struct sockaddr_in clientAddr = {};
-		socklen_t addrLen = sizeof(clientAddr);
+	LOG_DEBUG << "Accept connections on: " << serverSock;
 
-		// NOLINTNEXTLINE: Ignore reinterpret_cast warning, as it is necessary for the accept function
-		int clientSock = accept(m_serverSock, reinterpret_cast<struct sockaddr*>(&clientAddr), &addrLen);
-
-		if (clientSock < 0) {
-			if (errno == EAGAIN || errno == EWOULDBLOCK)
-				break; // No more pending connections
-			std::cerr << "error: accept: " << strerror(errno) << "\n";
-			break;
-		}
-
-		// Add client socket to epoll instance
-		struct epoll_event event = {};
-		event.events = EPOLLIN;
-		event.data.fd = clientSock;
-		if (epoll_ctl(m_epfd, EPOLL_CTL_ADD, clientSock, &event) < 0) {
-			std::cerr << "error: epoll_ctl: clientSock: " << strerror(errno) << "\n";
-			close(clientSock);
-		}
+	if ((eventMask & EPOLLIN) == 0) {
+		LOG_ERROR << "Received unknown event:" << eventMask;
+		return;
 	}
+
+	while (true) {
+		struct sockaddr_storage clientAddr = {};
+		socklen_t clientLen = sizeof(clientAddr);
+
+		// NOLINTNEXTLINE: we need to use reinterpret_cast to convert sockaddr_storage to sockaddr
+		struct sockaddr* addrCast = reinterpret_cast<struct sockaddr*>(&clientAddr);
+
+		const int clientFd = accept(serverSock.fd, addrCast, &clientLen);
+		if (clientFd == -1) {
+			if (errno == EAGAIN || errno == EWOULDBLOCK)
+				return; // No more pending connections
+			LOG_ERROR << "accept(): " << strerror(errno);
+			continue;
+		}
+
+		const Socket clientSock = retrieveSocketInfo(clientFd, addrCast, clientLen);
+		if (clientSock.fd == -1)
+			continue;
+
+		if (!registerConnection(serverSock, clientSock))
+			continue;
+	}
+}
+
+bool Server::registerConnection(const Socket& serverSock, const Socket& clientSock)
+{
+	struct epoll_event event = {};
+	event.events = EPOLLIN;
+	event.data.fd = clientSock.fd;
+
+	if (!addEvent(m_epfd, clientSock.fd, &event)) {
+		close(clientSock.fd);
+		LOG_ERROR << "Failed to add event for " << clientSock;
+		return false;
+	}
+
+	m_connections.insert(std::make_pair(clientSock.fd, Connection(serverSock, clientSock)));
+
+	LOG_INFO << "New Connection: " << clientSock << " for server: " << serverSock;
+	return true;
 }
 
 /**
@@ -200,9 +294,9 @@ void Server::acceptConnection() const
  *      to the client socket using the write function.
  *
  */
-void Server::handleConnections(int clientSock, RequestParser& parser)
+void Server::handleConnections(const Connection& connection)
 {
-	LOG_DEBUG << "Handling connection on fd " << clientSock;
+	LOG_DEBUG << "Handling connection: " << connection.getClient() << " for server: " << connection.getServer();
 	// Handle client data
 	char buffer[BUFFER_SIZE];
 	HTTPRequest request;
@@ -211,37 +305,28 @@ void Server::handleConnections(int clientSock, RequestParser& parser)
 	request.httpStatus = StatusOK;
 	request.shallCloseConnection = false;
 
-	Location location = {};
-	location.path = "/";
-	location.root = "/workspaces/webserv";
-	location.index = "index.html";
-
-	ServerConfig serverConfig;
-	serverConfig.locations.push_back(location);
-
-	ConfigFile configFile;
-	configFile.serverConfigs.push_back(serverConfig);
-
-	const ssize_t bytesRead = read(clientSock, buffer, BUFFER_SIZE);
+	const ssize_t bytesRead = recv(connection.getClient().fd, buffer, BUFFER_SIZE, 0);
 	if (bytesRead < 0) {
 		std::cerr << "error: read\n";
-		close(clientSock);
+		close(connection.getClient().fd);
 	} else if (bytesRead == 0) {
 		// Connection closed by client
-		close(clientSock);
+		close(connection.getClient().fd);
 	} else {
-		m_requestStrings[clientSock] += buffer;
-		if (checkForCompleteRequest(clientSock)) {
-			LOG_DEBUG << "Received complete request: " << '\n' << m_requestStrings[clientSock];
+		m_requestStrings[connection.getClient().fd] += buffer;
+		if (checkForCompleteRequest(connection.getClient().fd)) {
+			LOG_DEBUG << "Received complete request: " << '\n' << m_requestStrings[connection.getClient().fd];
 			try {
-				parser.parseHttpRequest(m_requestStrings[clientSock], request);
-				parser.clearParser();
+				m_requestParser.parseHttpRequest(m_requestStrings[connection.getClient().fd], request);
+				m_requestParser.clearParser();
 			} catch (std::exception& e) {
 				std::cerr << "Error: " << e.what() << std::endl;
 			}
-			ResponseBuilder builder(configFile);
-			builder.buildResponse(request);
-			send(clientSock, builder.getResponse().c_str(), builder.getResponse().size(), 0 );
+			m_responseBuilder.buildResponse(request);
+			send(connection.getClient().fd, m_responseBuilder.getResponse().c_str(),
+				m_responseBuilder.getResponse().size(), 0);
+		} else {
+			LOG_DEBUG << "Received partial request: " << '\n' << m_requestStrings[connection.getClient().fd];
 		}
 	}
 }
@@ -251,22 +336,102 @@ bool Server::checkForCompleteRequest(int clientSock)
 	const size_t headerEndPos = m_requestStrings[clientSock].find("\r\n\r\n");
 
 	return (headerEndPos != std::string::npos);
-		/*
-		headerEndPos += 4;
-		size_t bodySize = m_requestStrings[clientSock].size() - headerEndPos;
-		// FIXME: add check against default/config max body size
-		size_t contentLengthPos = m_requestStrings[clientSock].find("Content-Length");
-		size_t transferEncodingPos = m_requestStrings[clientSock].find("Transfer-Encoding");
+	/*
+	headerEndPos += 4;
+	size_t bodySize = m_requestStrings[clientSock].size() - headerEndPos;
+	// FIXME: add check against default/config max body size
+	size_t contentLengthPos = m_requestStrings[clientSock].find("Content-Length");
+	size_t transferEncodingPos = m_requestStrings[clientSock].find("Transfer-Encoding");
 
-		if (contentLengthPos != std::string::npos && transferEncodingPos == std::string::npos) {
-			unsigned long contentLength
-				= std::strtoul(m_requestStrings[clientSock].c_str() + contentLengthPos + 15, NULL, 10);
-			if (bodySize >= contentLength)
-				return true;
-		} else if (transferEncodingPos != std::string::npos) {
-			std::string tmp = m_requestStrings[clientSock].substr(transferEncodingPos);
-			if (tmp.find("chunked") != std::string::npos && tmp.find("0\r\n\r\n") != std::string::npos)
-				return true;
-		}
-		*/
+	if (contentLengthPos != std::string::npos && transferEncodingPos == std::string::npos) {
+		unsigned long contentLength
+			= std::strtoul(m_requestStrings[clientSock].c_str() + contentLengthPos + 15, NULL, 10);
+		if (bodySize >= contentLength)
+			return true;
+	} else if (transferEncodingPos != std::string::npos) {
+		std::string tmp = m_requestStrings[clientSock].substr(transferEncodingPos);
+		if (tmp.find("chunked") != std::string::npos && tmp.find("0\r\n\r\n") != std::string::npos)
+			return true;
+	}
+	*/
+}
+
+int createListeningSocket(struct addrinfo* addrinfo, int backlog)
+{
+	addrinfo->ai_socktype |= SOCK_NONBLOCK;
+
+	const int newFd = socket(addrinfo->ai_family, addrinfo->ai_socktype, addrinfo->ai_protocol);
+	if (newFd == -1) {
+		LOG_DEBUG << "socket(): " << strerror(errno);
+		return -1;
+	}
+
+	int reuse = 1;
+	if (-1 == setsockopt(newFd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int))) {
+		close(newFd);
+		LOG_DEBUG << "setsockopt(): " << strerror(errno);
+		return -1;
+	}
+
+	if (-1 == bind(newFd, addrinfo->ai_addr, addrinfo->ai_addrlen)) {
+		close(newFd);
+		LOG_DEBUG << "bind(): " << strerror(errno);
+		return -1;
+	}
+
+	if (-1 == listen(newFd, backlog)) {
+		close(newFd);
+		LOG_DEBUG << "listen(): " << strerror(errno);
+		return -1;
+	}
+	return newFd;
+}
+
+Socket retrieveSocketInfo(const int sockFd, const struct sockaddr* sockaddr, socklen_t socklen)
+{
+	char bufferHost[NI_MAXHOST];
+	char bufferPort[NI_MAXSERV];
+
+	const int ret = getnameinfo(
+		sockaddr, socklen, bufferHost, NI_MAXHOST, bufferPort, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+	if (ret != 0) {
+		LOG_ERROR << "getnameinfo(): " << gai_strerror(ret);
+		close(sockFd);
+		const Socket errSock = { -1, "", "" };
+		return (errSock);
+	}
+
+	const Socket newSock = { sockFd, bufferHost, bufferPort };
+
+	return (newSock);
+}
+
+bool addEvent(int epfd, int newfd, epoll_event* event)
+{
+	if (-1 == epoll_ctl(epfd, EPOLL_CTL_ADD, newfd, event)) {
+		LOG_ERROR << "epoll_ctl: EPOLL_CTL_ADD: " << strerror(errno);
+		return false;
+	}
+	LOG_DEBUG << "epoll_ctl: Added new fd: " << newfd;
+
+	return true;
+}
+
+void removeEvent(int epfd, int delfd)
+{
+	if (-1 == epoll_ctl(epfd, EPOLL_CTL_DEL, delfd, NULL)) {
+		LOG_ERROR << "epoll_ctl: EPOLL_CTL_DEL: " << strerror(errno);
+		return;
+	}
+	LOG_DEBUG << "epoll_ctl: Removed fd: " << delfd;
+}
+
+bool modifyEvent(int epfd, int modfd, epoll_event* event)
+{
+	if (-1 == epoll_ctl(epfd, EPOLL_CTL_MOD, modfd, event)) {
+		LOG_ERROR << "epoll_ctl: EPOLL_CTL_MOD: " << strerror(errno);
+		return false;
+	}
+	LOG_DEBUG << "epoll_ctl: Modified fd: " << modfd;
+	return true;
 }
