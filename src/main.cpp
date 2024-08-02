c21afe1c7f52d6aa698d890b5750b48fd91ce136
Gabriel Wolf <gwolf@student.42vienna.com>
2024-08-02 09:56:55 +0000

    temp: create dummy config

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,49 @@
+#include "ConfigFile.hpp"
+#include "Log.hpp"
+#include "LogData.hpp"
 #include "LogOutputterConsole.hpp"
 #include "LogOutputterFile.hpp"
+#include "ResponseBuilder.hpp"
 #include "Server.hpp"
-#include "Log.hpp"
-#include "ConfigFile.hpp"
 #include <cerrno>
-#include "Dispatcher.hpp"
+
+ConfigFile createDummyConfig()
+{
+	Location location1 = {};
+	location1.path = "/";
+	location1.root = "/workspaces/webserv";
+	location1.index = "index.html";
+
+	ServerConfig serverConfig8080;
+	serverConfig8080.locations.push_back(location1);
+	serverConfig8080.port = 8080;
+	serverConfig8080.host = "127.0.0.1";
+	serverConfig8080.serverName = "root";
+
+	Location location2 = {};
+	location2.path = "/";
+	location2.root = "/workspaces/webserv/doc";
+	location2.index = "index.html";
+
+	ServerConfig serverConfig8090;
+	serverConfig8090.locations.push_back(location2);
+	serverConfig8090.port = 8090;
+	serverConfig8090.host = "127.0.0.1";
+	serverConfig8090.serverName = "doc";
+
+	ServerConfig serverConfig8090dupl;
+	serverConfig8090dupl.locations.push_back(location2);
+	serverConfig8090dupl.port = 8090;
+	serverConfig8090dupl.host = "127.0.0.1";
+	serverConfig8090dupl.serverName = "duplicate";
+
+	ConfigFile configFile;
+	configFile.serverConfigs.push_back(serverConfig8080);
+	configFile.serverConfigs.push_back(serverConfig8090);
+	configFile.serverConfigs.push_back(serverConfig8090dupl);
+
+	return configFile;
+}
 
 int main(int argc, char** argv)
 {
@@ -15,27 +54,13 @@ int main(int argc, char** argv)
 	}
 	static_cast<void>(argv);
 
-	Location location = {};
-	location.path = "/";
-	location.root = "/workspaces/webserv";
-	location.index = "index.html";
-
-	ServerConfig serverConfig;
-	serverConfig.locations.push_back(location);
-	serverConfig.port = 8080;
-	serverConfig.host = "localhost";
-
-	ConfigFile configFile;
-	configFile.serverConfigs.push_back(serverConfig);
-
+	ConfigFile configFile = createDummyConfig();
+	
 	weblog::initConsole(weblog::LevelDebug);
-	try{
-		Dispatcher dispatcher(configFile);
-		LOG_DEBUG << "Add new server";
-		dispatcher.addListeningEndpoint("*", 10, "8080");
-		dispatcher.handleEvents();
-	}
-	catch (std::exception& e){
+	try {
+		Server server(configFile, -1);
+		server.run();
+	} catch (std::exception& e) {
 		LOG_ERROR << e.what();
 		return 1;
 	}
