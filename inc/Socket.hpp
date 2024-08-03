7588f79609ef0e5b2d73fd2c9791364615acd994
Gabriel Wolf <gwolf@student.42vienna.com>
2024-08-03 17:20:44 +0000

    format: add Doxygen

--- inc/Socket.hpp
+++ inc/Socket.hpp
@@ -2,10 +2,20 @@
 
 #include <string>
 
+/**
+ * @brief Represent a socket connection.
+ */
 struct Socket {
 	int fd;
 	std::string host;
 	std::string port;
 };
 
+/**
+ * @brief Output the socket information to the output stream.
+ *
+ * @param ostream Output stream
+ * @param socket Socket to output
+ * @return std::ostream& Output stream
+ */
 std::ostream& operator<<(std::ostream& ostream, const Socket& socket);
