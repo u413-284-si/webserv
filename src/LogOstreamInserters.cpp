98125a8cbbbd3e7e67e739ee213ddec839455f7c
Gabriel Wolf <gwolf@student.42vienna.com>
2024-08-04 11:24:03 +0000

    feat: add ostream inserters for enum StatusCode; URI; HTTPRequest

--- src/LogOstreamInserters.cpp
+++ src/LogOstreamInserters.cpp
@@ -99,3 +99,81 @@ std::ostream& operator<<(std::ostream& ostream, Method method)
 	}
 	return ostream;
 }
+
+/**
+ * @brief Overload << operator to append a statusCode.
+ *
+ * @param ostream The output stream.
+ * @param statusCode The statusCode enum.
+ * @return std::ostream& The output stream.
+ */
+std::ostream& operator<<(std::ostream& ostream, statusCode statusCode)
+{
+	switch (statusCode) {
+	case StatusOK:
+		ostream << "200 OK";
+		break;
+	case StatusMovedPermanently:
+		ostream << "301 Moved Permanently";
+		break;
+	case StatusBadRequest:
+		ostream << "400 Bad Request";
+		break;
+	case StatusForbidden:
+		ostream << "403 Forbidden";
+		break;
+	case StatusNotFound:
+		ostream << "404 Not Found";
+		break;
+	case StatusMethodNotAllowed:
+		ostream << "405 Method Not Allowed";
+		break;
+	case StatusInternalServerError:
+		ostream << "500 Internal Server Error";
+		break;
+	case StatusMethodNotImplemented:
+		ostream << "501 Method Not Implemented";
+		break;
+	case StatusNonSupportedVersion:
+		ostream << "505 HTTP Version Not Supported";
+		break;
+	}
+	return ostream;
+}
+
+/**
+ * @brief Overload << operator to append a URI.
+ *
+ * @param ostream The output stream.
+ * @param uri The URI object.
+ * @return std::ostream& The output stream.
+ */
+std::ostream& operator<<(std::ostream& ostream, const URI& uri)
+{
+	ostream << "Path: " << uri.path << '\n';
+	ostream << "Query: " << uri.query << '\n';
+	ostream << "Fragment: " << uri.fragment << '\n';
+	return ostream;
+}
+
+/**
+ * @brief Overload << operator to append a HTTPRequest.
+ *
+ * @param ostream The output stream.
+ * @param httpRequest The HTTPRequest object.
+ * @return std::ostream& The output stream.
+ */
+std::ostream& operator<<(std::ostream& ostream, const HTTPRequest& httpRequest)
+{
+	ostream << "Method: " << httpRequest.method << '\n';
+	ostream << "URI:\n" << httpRequest.uri;
+	ostream << "Version: " << httpRequest.version << '\n';
+	ostream << "Headers:\n";
+	for (std::map<std::string, std::string>::const_iterator it = httpRequest.headers.begin();
+		 it != httpRequest.headers.end(); ++it)
+		ostream << "  " << it->first << ": " << it->second << '\n';
+	ostream << "Body: " << httpRequest.body << '\n';
+	ostream << "HTTP status: " << httpRequest.httpStatus << '\n';
+	ostream << "Shall close connection: " << httpRequest.shallCloseConnection << '\n';
+	return ostream;
+}
