8e7bb06ae0876509ea3dc02537c36bdfd575540d
Gabriel Wolf <gwolf@student.42vienna.com>
2024-08-04 13:03:22 +0000

    feat: implement tests for current ostream inserters

--- test/test_OstreamInserters.cpp
+++ test/test_OstreamInserters.cpp
@@ -50,98 +50,174 @@ TEST(OstreamInserters, StatusCode)
 	EXPECT_EQ(ostream.str(), "505 HTTP Version Not Supported");
 }
 
-TEST(OstreamInserters, URI)
+// sets default values for all struct which get printed
+class OstreamInsertersTest : public ::testing::Test {
+	protected:
+
+	LimitExcept m_limitExcept = {
+		.allowedMethods = { true, true, false },
+		.allow = "127.0.0.1",
+		.deny = "192.168.0.1"
+	};
+
+	Location m_location = {
+		.path = "/path/to/resource",
+		.root = "/root",
+		.index = "index.html",
+		.cgiExt = ".php",
+		.cgiPath = "/cgi-bin",
+		.isAutoindex = true,
+		.limitExcept = m_limitExcept,
+		.returns = {
+			{ StatusOK, "OK.html" },
+			{ StatusBadRequest, "BadRequest.html" }
+		}
+	};
+
+	ServerConfig m_server = {
+		.serverName = "localhost",
+		.host = "10.11.12.13",
+		.port = 8080,
+		.maxBodySize = 1024,
+		.errorPage = {
+			{ StatusBadRequest, "BadRequest.html" },
+			{ StatusForbidden, "Forbidden.html" }
+		},
+		.locations = { m_location }
+	};
+
+	ConfigFile m_configFile = {
+		.serverConfigs = { m_server }
+	};
+
+	URI m_uri = {
+		.path = "/path/to/resource",
+		.query = "query=value",
+		.fragment = "fragment"
+	};
+
+	HTTPRequest m_httpRequest = {
+		.method = MethodGet,
+		.uri = m_uri,
+		.version = "HTTP/1.1",
+		.headers = {
+			{ "Host", "localhost" },
+			{ "User-Agent", "curl/7.68.0" }
+		},
+		.body = "Hello, World!",
+		.httpStatus = StatusOK,
+		.shallCloseConnection = false
+	};
+
+	HTTPResponse m_httpResponse = {
+		.status = StatusOK,
+		.targetResource = "/path/to/resource",
+		.body = "<html><body><h1>Hello, World!</h1></body></html>",
+		.location = m_server.locations.begin(),
+		.method = MethodGet,
+		.isAutoindex = true
+	};
+};
+
+TEST_F(OstreamInsertersTest, Location)
 {
 	std::ostringstream ostream;
-	URI uri;
-	uri.path = "/path/to/resource";
-	uri.query = "query=value";
-	uri.fragment = "fragment";
-	ostream << uri;
+	ostream << m_location;
+
+	const std::string expected =
+	"Path: /path/to/resource\n"
+	"Root: /root\n"
+	"Index: index.html\n"
+	"CGI extension: .php\n"
+	"CGI path: /cgi-bin\n"
+	"Autoindex: 1\n"
+	"LimitExcept:\n"
+	"  Allowed methods:\n"
+	"    GET\n"
+	"    POST\n"
+	"  Allow: 127.0.0.1\n"
+	"  Deny: 192.168.0.1\n"
+	"Returns:\n"
+	"  200: OK.html\n"
+	"  400: BadRequest.html\n";
+
+	EXPECT_EQ(ostream.str(), expected);
+}
+
+TEST_F(OstreamInsertersTest, ServerConfig)
+{
+	std::ostringstream ostream;
+	ostream << m_server;
+
+	std::ostringstream expected;
+	expected <<
+	"Server name: localhost\n"
+	"Host: 10.11.12.13\n"
+	"Port: 8080\n"
+	"Max body size: 1024\n"
+	"Error pages:\n"
+	"  400: BadRequest.html\n"
+	"  403: Forbidden.html\n"
+	"Locations:\n" << m_location;
+
+	EXPECT_EQ(ostream.str(), expected.str());
+}
+
+TEST_F(OstreamInsertersTest, ConfigFile)
+{
+	std::ostringstream ostream;
+	ostream << m_configFile;
+
+	std::ostringstream expected;
+	expected <<
+	"Config file\n"
+	<< m_server;
+}
+
+TEST_F(OstreamInsertersTest, URI)
+{
+	std::ostringstream ostream;
+	ostream << m_uri;
+
 	const std::string expected =
 	"  Path: /path/to/resource\n"
 	"  Query: query=value\n"
 	"  Fragment: fragment\n";
+
 	EXPECT_EQ(ostream.str(), expected);
 }
 
-TEST(OstreamInserters, HTTPRequest)
+TEST_F(OstreamInsertersTest, HTTPRequest)
 {
-	URI uri;
-	uri.path = "/path/to/resource";
-	uri.query = "query=value";
-	uri.fragment = "fragment";
-
-	HTTPRequest httpRequest;
-	httpRequest.method = MethodGet;
-	httpRequest.uri = uri;
-	httpRequest.version = "HTTP/1.1";
-	httpRequest.headers["Host"] = "localhost:8080";
-	httpRequest.headers["User-Agent"] = "curl/7.68.0";
-	httpRequest.body = "Hello, World!";
-	httpRequest.httpStatus = StatusOK;
-	httpRequest.shallCloseConnection = false;
-
 	std::ostringstream ostream;
-	ostream << httpRequest;
+	ostream << m_httpRequest;
 
 	std::ostringstream expected;
 	expected <<
-	"Method: GET\n"
-	"URI:\n" <<
-	uri <<
+	"Method: " << m_httpRequest.method << "\n"
+	"URI:\n" << m_uri <<
 	"Version: HTTP/1.1\n"
 	"Headers:\n"
-	"  Host: localhost:8080\n"
+	"  Host: localhost\n"
 	"  User-Agent: curl/7.68.0\n"
 	"Body: Hello, World!\n"
-	"HTTP status: 200 OK\n"
+	"HTTP status: " << m_httpRequest.httpStatus << "\n"
 	"Shall close connection: 0\n";
 	EXPECT_EQ(ostream.str(), expected.str());
 }
 
-TEST(OstreamInserters, HTTPResponse)
+TEST_F(OstreamInsertersTest, HTTPResponse)
 {
-	LimitExcept limitExcept;
-	limitExcept.allowedMethods[MethodGet] = true;
-	limitExcept.allowedMethods[MethodPost] = true;
-	limitExcept.allowedMethods[MethodDelete] = false;
-	limitExcept.allow = "127.0.0.1";
-	limitExcept.deny = "192.168.0.1";
-
-	Location location;
-	location.path = "/path/to/resource";
-	location.root = "/root";
-	location.index = "index.html";
-	location.cgiExt = ".php";
-	location.cgiPath = "/cgi-bin";
-	location.isAutoindex = true;
-	location.limitExcept = limitExcept;
-	location.returns[StatusOK] = "OK.html";
-	location.returns[StatusBadRequest] = "BadRequest.html";
-	std::vector<Location> locations;
-	locations.push_back(location);
-
-	HTTPResponse httpResponse;
-	httpResponse.status = StatusOK;
-	httpResponse.targetResource = "/path/to/resource";
-	httpResponse.body = "<html><body><h1>Hello, World!</h1></body></html>";
-	httpResponse.location = locations.begin();
-	httpResponse.method = MethodGet;
-	httpResponse.isAutoindex = true;
-
 	std::ostringstream ostream;
-	ostream << httpResponse;
+	ostream << m_httpResponse;
 
 	std::ostringstream expected;
 	expected <<
-	"Status code: 200 OK\n"
+	"Status code: " << m_httpResponse.status << "\n"
 	"Target resource: /path/to/resource\n"
 	"Body: <html><body><h1>Hello, World!</h1></body></html>\n"
-	"Location:\n" <<
-	location <<
-	"\n"
-	"Method: GET\n"
+	"Location:\n" << m_location << "\n"
+	"Method: " << m_httpResponse.method << "\n"
 	"Autoindex: 1\n";
 	EXPECT_EQ(ostream.str(), expected.str());
 }
