23fcddf2107baf0f4c0a54a3db3d5b291560631f
Gabriel Wolf <gwolf@student.42vienna.com>
2024-08-10 13:09:03 +0000

    fix: remove fd from socket

--- test/test_Server.cpp
+++ test/test_Server.cpp
@@ -15,8 +15,8 @@ TEST(checkDuplicateServer, EmptyMap)
 TEST(checkDuplicateServer, NoDuplicate)
 {
 	std::map<int, Socket> virtualServers;
-	virtualServers[1] = Socket{ 1, "127.0.0.1", "7070" };
-	virtualServers[2] = Socket{ 2, "192.168.0.1", "8080" };
+	virtualServers[1] = Socket { "127.0.0.1", "7070" };
+	virtualServers[2] = Socket { "192.168.0.1", "8080" };
 
 	std::string host = "127.0.0.1";
 	std::string port = "8080";
@@ -27,9 +27,9 @@ TEST(checkDuplicateServer, NoDuplicate)
 TEST(checkDuplicateServer, DuplicateServer)
 {
 	std::map<int, Socket> virtualServers;
-	virtualServers[1] = Socket{ 1, "127.0.0.1", "6060" };
-	virtualServers[2] = Socket{ 2, "127.0.0.1", "7070" };
-	virtualServers[3] = Socket{ 3, "127.0.0.1", "8080" };
+	virtualServers[1] = Socket { "127.0.0.1", "6060" };
+	virtualServers[2] = Socket { "127.0.0.1", "7070" };
+	virtualServers[3] = Socket { "127.0.0.1", "8080" };
 
 	std::string host = "127.0.0.1";
 	std::string port = "8080";
@@ -40,11 +40,11 @@ TEST(checkDuplicateServer, DuplicateServer)
 TEST(checkDuplicateServer, DuplicateServerLocalhost)
 {
 	std::map<int, Socket> virtualServers;
-	virtualServers[1] = Socket{ 1, "127.0.0.1", "6060" };
-	virtualServers[2] = Socket{ 2, "127.0.0.1", "7070" };
-	virtualServers[3] = Socket{ 3, "127.0.0.1", "8080" };
-	virtualServers[4] = Socket{ 4, "::1", "9090" };
-	virtualServers[5] = Socket{ 5, "::1", "1010" };
+	virtualServers[1] = Socket { "127.0.0.1", "6060" };
+	virtualServers[2] = Socket { "127.0.0.1", "7070" };
+	virtualServers[3] = Socket { "127.0.0.1", "8080" };
+	virtualServers[4] = Socket { "::1", "9090" };
+	virtualServers[5] = Socket { "::1", "1010" };
 
 	std::string host = "localhost";
 	std::string port = "7070";
