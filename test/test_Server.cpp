dfd72f447a289958649b331f3cbd7aa37ae08ef9
Gabriel Wolf <gwolf@student.42vienna.com>
2024-08-10 13:09:19 +0000

    feat: create test for adding vserver

--- test/test_Server.cpp
+++ test/test_Server.cpp
@@ -1,5 +1,8 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "MockEpollWrapper.hpp"
+#include "MockSocketPolicy.hpp"
 #include "Server.hpp"
 
 TEST(checkDuplicateServer, EmptyMap)
@@ -58,4 +61,36 @@ TEST(checkDuplicateServer, DuplicateServerLocalhost)
 	port = "1010";
 
 	EXPECT_EQ(checkDuplicateServer(virtualServers, host, port), true);
+}
+
+TEST(addVirtualServer, Success)
+{
+	std::map<int, Socket> virtualServers;
+	MockSocketPolicy socketPolicy;
+	MockEpollWrapper epollWrapper;
+	std::string host = "127.0.0.1";
+	std::string port = "8080";
+	const int backlog = 10;
+	struct addrinfo* addrinfo = new struct addrinfo;
+	*addrinfo = {
+		.ai_next = nullptr
+	};
+
+	EXPECT_CALL(socketPolicy, resolveListeningAddresses)
+	.Times(1)
+	.WillOnce(testing::Return(addrinfo));
+
+	EXPECT_CALL(socketPolicy, createListeningSocket)
+	.Times(1)
+	.WillOnce(testing::Return(1));
+
+	EXPECT_CALL(socketPolicy, retrieveSocketInfo)
+	.Times(1).
+	WillOnce(testing::Return(Socket { host, port }));
+
+	EXPECT_CALL(epollWrapper, addEvent)
+	.Times(1)
+	.WillOnce(testing::Return(true));
+
+	EXPECT_EQ(addVirtualServer(socketPolicy, epollWrapper, virtualServers, host, backlog, port), true);
 }
