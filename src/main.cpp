c82e20ed43c10f2b393b7efafb70abb6f3688d51
QCHR1581 <chris1581.at@gmail.com>
2024-09-02 13:53:52 +0000

    test: removed createDummyConfig function

--- src/main.cpp
+++ src/main.cpp
@@ -10,44 +10,6 @@
 #include "SocketPolicy.hpp"
 #include <cerrno>
 
-ConfigFile createDummyConfig()
-{
-	Location location1;
-	location1.path = "/";
-	location1.root = "/workspaces/webserv";
-	location1.indices.push_back("index.htm");
-
-	ConfigServer serverConfig8080;
-	serverConfig8080.locations.push_back(location1);
-	serverConfig8080.host = "127.0.0.1";
-	serverConfig8080.port = "8080";
-	serverConfig8080.serverName = "root";
-
-	Location location2;
-	location2.path = "/";
-	location2.root = "/workspaces/webserv/doc";
-	location2.indices.push_back("index.htm");
-
-	ConfigServer serverConfig8090;
-	serverConfig8090.locations.push_back(location2);
-	serverConfig8090.host = "127.0.0.1";
-	serverConfig8090.port = "8090";
-	serverConfig8090.serverName = "doc";
-
-	ConfigServer serverConfig8090dupl;
-	serverConfig8090dupl.locations.push_back(location2);
-	serverConfig8090dupl.host = "127.0.0.1";
-	serverConfig8090dupl.port = "8090";
-	serverConfig8090dupl.serverName = "duplicate";
-
-	ConfigFile configFile;
-	configFile.servers.push_back(serverConfig8080);
-	configFile.servers.push_back(serverConfig8090);
-	configFile.servers.push_back(serverConfig8090dupl);
-
-	return configFile;
-}
-
 int main(int argc, char** argv)
 {
 	if (argc != 2) {
@@ -55,18 +17,17 @@ int main(int argc, char** argv)
 		std::cerr << program_invocation_name << " <config file>\n";
 		return 1;
 	}
-	static_cast<void>(argv);
-
-	ConfigFile configFile = createDummyConfig();
 
 	weblog::initConsole(weblog::LevelDebug);
 	try {
 		EpollWrapper epollWrapper(10, -1);
 		SocketPolicy socketPolicy;
+
+		ConfigFileParser parser;
+		ConfigFile configFile = parser.parseConfigFile(argv[1]);
+
 		Server server(configFile, epollWrapper, socketPolicy);
 		initVirtualServers(server, 10, server.getServerConfigs());
-		ConfigFileParser parser;
-		parser.parseConfigFile(argv[1]);
 		server.run();
 	} catch (std::exception& e) {
 		LOG_ERROR << e.what();
