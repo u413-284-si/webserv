606f5b9a3762c6f0906281b6b36b9b56a6d20ec9
Sirui <sirui.qiu01@gmail.com>
2024-09-26 13:27:57 +0000

    fix: adapt CGIHandlerTest

--- test/test_CGIHandler.cpp
+++ test/test_CGIHandler.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <string>
+#include <vector>
 
 #include "CGIHandler.hpp"
 #include "ConfigFile.hpp"
@@ -10,13 +11,13 @@
 class CGIHandlerTest : public ::testing::Test {
 protected:
 	CGIHandlerTest()
-		: m_cgiHandler(m_cgiPath, m_cgiExt)
 	{
 		request.uri.path = "/cgi-bin/test.py/some/more/path";
 		request.uri.query = "name=John&age=25";
 		request.headers["Content-Length"] = "20";
 		request.headers["Content-Type"] = "text/plain";
 		request.method = MethodPost;
+		connection.m_request = request;
 
 		location.cgiExt = ".py";
 		location.cgiPath = "/usr/bin/python3";
@@ -24,56 +25,68 @@ protected:
 		location.root = "/var/www/html";
 
 		locations.push_back(location);
-		iter = locations.begin();
+		connection.location = locations.begin();
+
+		serverConfig.host = serverSock.host;
+		serverConfig.port = serverSock.port;
+		configFile.servers.push_back(serverConfig);
 	}
 	~CGIHandlerTest() override { }
 
-	std::string m_cgiPath = "/usr/bin/python3";
-	std::string m_cgiExt = ".py";
 	const Socket clientSock = { "1.23.4.56", "36952" };
 	const Socket serverSock = { "127.0.0.1", "8080" };
+	const int dummyFd = 10;
 	HTTPRequest request;
 	Location location;
 	std::vector<Location> locations;
-	std::vector<Location>::const_iterator iter;
 
-	CGIHandler m_cgiHandler;
+	Connection connection = Connection(serverSock, clientSock, dummyFd, configFile.servers);
+	ConfigServer serverConfig;
+	ConfigFile configFile;
 };
 
-TEST_F(CGIHandlerTest, Init)
+TEST_F(CGIHandlerTest, Ctor)
 {
 	// Arrange
+	CGIHandler cgiHandler(connection);
 
 	// Act
-	m_cgiHandler.init(clientSock, serverSock, request, iter);
+	const std::vector<std::string> env = cgiHandler.getEnv();
 
 	// Assert
-	EXPECT_EQ(m_cgiHandler.getCGIPath(), "/usr/bin/python3");
-	EXPECT_EQ(m_cgiHandler.getCGIExt(), ".py");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("CONTENT_LENGTH"), "20");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("CONTENT_TYPE"), "text/plain");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("PATH_INFO"), "/some/more/path");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("PATH_TRANSLATED"), "/var/www/html/some/more/path");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("QUERY_STRING"), "name=John&age=25");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("REMOTE_ADDR"), "1.23.4.56");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("REMOTE_PORT"), "36952");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("REQUEST_METHOD"), "POST");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("REQUEST_URI"), "/cgi-bin/test.py/some/more/path?name=John&age=25");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("SCRIPT_NAME"), "/cgi-bin/test.py");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("SCRIPT_FILENAME"), "/var/www/html/cgi-bin/test.py");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("SERVER_ADDR"), "127.0.0.1");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("SERVER_PORT"), "8080");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("SYSTEM_ROOT"), "/var/www/html");
+	EXPECT_EQ(cgiHandler.getCGIPath(), "/usr/bin/python3");
+	EXPECT_EQ(cgiHandler.getCGIExt(), ".py");
+
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "CONTENT_LENGTH=20"), "CONTENT_LENGTH=20");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "CONTENT_TYPE=text/plain"), "CONTENT_TYPE=text/plain");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "GATEWAY_INTERFACE=CGI/1.1"), "GATEWAY_INTERFACE=CGI/1.1");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "PATH_INFO=/some/more/path"), "PATH_INFO=/some/more/path");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "PATH_TRANSLATED=/var/www/html/some/more/path"), "PATH_TRANSLATED=/var/www/html/some/more/path");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "QUERY_STRING=name=John&age=25"), "QUERY_STRING=name=John&age=25");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "REDIRECT_STATUS=200"), "REDIRECT_STATUS=200");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "REMOTE_ADDR=1.23.4.56"), "REMOTE_ADDR=1.23.4.56");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "REMOTE_PORT=36952"), "REMOTE_PORT=36952");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "REQUEST_METHOD=POST"), "REQUEST_METHOD=POST");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "REQUEST_URI=/cgi-bin/test.py/some/more/path?name=John&age=25"), "REQUEST_URI=/cgi-bin/test.py/some/more/path?name=John&age=25");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "SCRIPT_NAME=/cgi-bin/test.py"), "SCRIPT_NAME=/cgi-bin/test.py");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "SCRIPT_FILENAME=/var/www/html/cgi-bin/test.py"), "SCRIPT_FILENAME=/var/www/html/cgi-bin/test.py");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "SERVER_ADDR=127.0.0.1"), "SERVER_ADDR=127.0.0.1");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "SERVER_NAME=127.0.0.1"), "SERVER_NAME=127.0.0.1");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "SERVER_PORT=8080"), "SERVER_PORT=8080");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "SYSTEM_ROOT=/var/www/html"), "SYSTEM_ROOT=/var/www/html");
 }
 
 TEST_F(CGIHandlerTest, NoPathInfo)
 {
 	// Arrange
 	request.uri.path = "/cgi-bin/test.py";
+	connection.m_request = request;
+	CGIHandler cgiHandler(connection);
+
 	// Act
-	m_cgiHandler.init(clientSock, serverSock, request, iter);
+	const std::vector<std::string> env = cgiHandler.getEnv();
 
 	// Assert
-	EXPECT_EQ(m_cgiHandler.getEnv().at("PATH_INFO"), "");
-	EXPECT_EQ(m_cgiHandler.getEnv().at("PATH_TRANSLATED"), "/var/www/html");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "PATH_INFO="), "PATH_INFO=");
+	EXPECT_EQ(*std::find(env.begin(), env.end(), "PATH_TRANSLATED=/var/www/html"), "PATH_TRANSLATED=/var/www/html");
 }
