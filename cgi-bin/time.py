50c8acb475d9f6814137fb8a553cbc694c1def4d
sirui <sirui.qiu01@gmail.com>
2024-10-26 18:23:16 +0000

    fix: Content-Type header typo

--- cgi-bin/time.py
+++ cgi-bin/time.py
@@ -3,7 +3,7 @@
 import datetime
 
 print("HTTP/1.1 200 OK")
-print("Content-type: text/html\r\n\r\n")
+print("Content-Type: text/html\r\n\r\n")
 print("<html>")
 print("<head>")
 print(datetime.datetime.strftime(datetime.datetime.now(), "<h1>  %H:%M:%S </h1>"))
