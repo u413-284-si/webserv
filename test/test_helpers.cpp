f125cd54372500ed5b3022820b18471eda98534c
Gabriel Wolf <gwolf@student.42vienna.com>
2024-12-09 12:38:26 +0000

    fix: remove default location

--- test/test_helpers.cpp
+++ test/test_helpers.cpp
@@ -2,14 +2,9 @@
 
 ConfigFile createTestConfigfile()
 {
-	Location location;
-	location.path = "/";
-	location.root = "/www/data";
-
 	ConfigServer server;
 	server.host = "127.0.0.1";
 	server.port = "8080";
-	server.locations.emplace_back(location);
 
 	ConfigFile configFile;
 	configFile.servers.emplace_back(server);
