84bcb46d3a49638495a88720ef482cf3f86c53a1
Gabriel Wolf <gwolf@student.42vienna.com>
2024-12-11 11:26:50 +0000

    feat: add util to replace alias in string

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -15,3 +15,34 @@ TEST(getFileExtension, Extension)
 	EXPECT_EQ(webutils::getFileExtension("a/path/to/file.txt"), "txt");
 	EXPECT_EQ(webutils::getFileExtension("a/path/with.inbetween/to/file.txt"), "txt");
 }
+
+TEST(replaceAlias, AliasFound)
+{
+	EXPECT_EQ(webutils::replaceAlias("/path/to/somewhere", "/path", "/replacement"), "/replacement/to/somewhere");
+	EXPECT_EQ(webutils::replaceAlias("/path/to/somewhere", "/path/to/somewhere", "/replacement"), "/replacement");
+}
+
+TEST(replaceAlias, AliasNotFound)
+{
+	EXPECT_EQ(webutils::replaceAlias("/path/to/somewhere", "/alias", "/replacement"), "");
+}
+
+TEST(replaceAlias, AliasNotFoundAtBeginning)
+{
+	EXPECT_EQ(webutils::replaceAlias("/path/to/somewhere", "/somewhere", "/replacement"), "");
+}
+
+TEST(replaceAlias, InputIsEmpty)
+{
+	EXPECT_EQ(webutils::replaceAlias("", "/path", "/replacement"), "");
+}
+
+TEST(replaceAlias, AliasIsEmpty)
+{
+	EXPECT_EQ(webutils::replaceAlias("/path/to/somewhere", "", "/replacement"), "/replacement/path/to/somewhere");
+}
+
+TEST(replaceAlias, ReplacementIsEmpty)
+{
+	EXPECT_EQ(webutils::replaceAlias("/path/to/somewhere", "/path", ""), "/to/somewhere");
+}
