0d65ab0883f52d4e2bb89ea6924f069b7dc6b830
Gabriel Wolf <gwolf@student.42vienna.com>
2024-12-12 18:53:49 +0000

    fix: rename macros and adapt doxygen

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,13 @@
 /**
  * @brief Stringize the result of expansion of a macro argument
  *
- * If the macro DEFAULT_CONFIG_PATH is defined with -D at compile time the literal value would be inserted. For it to be
- * a string the user would have to escape double quotes. These two macro stringize the defined value. The user can
- * redefine the path as "-D DEFAULT_CONFIG_PATH=./new/path"
+ * If the macro DEFAULT_CONFIG_PATH is defined with -D at compile time the literal value would be inserted. Since it has
+ * to be a string one would have to put the vlue in escaped double quotes. This macro in combination with STRINGIZE(s)
+ * stringize the defined value. First the macro gets expanded, and then the expanded value gets stringized. One can
+ * simply redefine the path with "-D DEFAULT_CONFIG_PATH=./new/path"
  * @sa https://gcc.gnu.org/onlinedocs/cpp/Stringizing.html
  */
-#define xstr(s) str(s)
+#define XSTRINGIZE(s) STRINGIZE(s)
 
 /**
  * @brief Converts macro argument into a string constant.
@@ -17,7 +18,7 @@
  * Uses the '#' preprocessing operator. When a macro parameter is used with a leading '#', the preprocessor replaces it
  * with the literal text of the actual argument
  */
-#define str(s) #s
+#define STRINGIZE(s) #s
 
 #ifndef DEFAULT_CONFIG_PATH
 #define DEFAULT_CONFIG_PATH ./config_files/standard_config.conf
@@ -32,7 +33,7 @@ int main(const int argc, const char* argv[])
 	}
 
 	// NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-	const std::string configFilePath = (argc == 1) ? xstr(DEFAULT_CONFIG_PATH) : argv[1];
+	const std::string configFilePath = (argc == 1) ? XSTRINGIZE(DEFAULT_CONFIG_PATH) : argv[1];
 	std::cout << "Config file path: " << configFilePath << '\n';
 
 	weblog::initConsole(weblog::LevelDebug);
