8f2547969e5c6f5743e729af7b99417b3eef356d
Gabriel Wolf <gwolf@student.42vienna.com>
2024-12-14 13:24:01 +0000

    fix: reorder variables

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -5,11 +5,11 @@ import os
 
 def pytest_addoption(parser):
     print("Registering custom pytest options")
+    parser.addoption("--server-executable", action="store", default="/workspaces/webserv/webserv", help="Path to the server executable")
+    parser.addoption("--config-file", action="store", default="/workspaces/webserv/config_files/valid_config.conf", help="Path to the server config file")
     parser.addoption("--with-coverage", action="store_true", help="Run server with kcov for coverage")
     parser.addoption("--kcov-output-dir", action="store", default="/workspaces/webserv/.vscode/coverage", help="Path to the kcov output directory")
     parser.addoption("--kcov-excl-path", action="store", default="--exclude-path=/usr/include,/usr/lib,/usr/local", help="Path to exclude from kcov coverage")
-    parser.addoption("--server-executable", action="store", default="/workspaces/webserv/webserv", help="Path to the server executable")
-    parser.addoption("--config-file", action="store", default="/workspaces/webserv/config_files/valid_config.conf", help="Path to the server config file")
 
 # Fixture to start the server for normal tests
 @pytest.fixture(scope="session")
@@ -21,8 +21,8 @@ def start_cpp_server(request):
     with_coverage = request.config.getoption("--with-coverage")
 
     # Path to the C++ server executable and kcov output
-    kcov_output_dir = request.config.getoption("--kcov-output-dir")
     kcov_excl_path = request.config.getoption("--kcov-excl-path")
+    kcov_output_dir = request.config.getoption("--kcov-output-dir")
 
     if with_coverage:
         # Create the kcov output directory if it doesn't exist
