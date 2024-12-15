c748d819086702ebb14cfaf1f2225112f1e2bdcd
Gabriel Wolf <gwolf@student.42vienna.com>
2024-12-15 09:18:08 +0000

    feat: prepare tests for POST

---
+++ test/integration/general/test_POST.py
@@ -0,0 +1,25 @@
+import requests
+import os
+
+# def test_POST_simple(start_cpp_server):
+#     print("Request for /uploads/test01")
+#     # Body to send
+#     payload = "Beam me up, Scotty!"
+#     response = requests.post("http://localhost:8080/uploads/test01.txt", data=payload)
+#     assert response.status_code == 201
+#     # Delete created file
+#     os.remove("workspaces/webserv/uploads/test01")
+
+# For encoding chunked
+# sa https://requests.readthedocs.io/en/latest/user/advanced/#chunk-encoded-requests
+# def generate_chunks():
+#     yield 'First chunk of data'.encode("utf-8")
+#     yield 'Second chunk of data'.encode("utf-8")
+#     yield 'Third chunk of data'.encode("utf-8")
+
+# def test_POST_chunked_encoding(start_cpp_server):
+#     print("Chunked Request for /uploads/test01")
+#     response = requests.post("http://localhost:8080/uploads/test01.txt", data=generate_chunks())
+#     assert response.status_code == 201
+#     # Delete created file
+#     os.remove("workspaces/webserv/uploads/test01")
