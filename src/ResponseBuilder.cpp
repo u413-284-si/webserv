ba12fcc984bf4309ea9abd0aba21ae43b0422bcb
Sirui <sirui.qiu01@gmail.com>
2024-12-22 21:16:45 +0000

    refactor: add parsed headers to final response

--- src/ResponseBuilder.cpp
+++ src/ResponseBuilder.cpp
@@ -53,10 +53,10 @@ void ResponseBuilder::buildResponse(Connection& connection)
 	parseResponseBody(request);
 
 	appendStatusLine(request);
-	if (request.hasCGI && request.httpStatus == StatusOK)
-		appendHeadersCGI(request);
-	else
-		appendHeaders(request);
+	// if (request.hasCGI && request.httpStatus == StatusOK)
+	// 	appendHeadersCGI(request);
+	// else
+	appendHeaders(request);
 
 	LOG_DEBUG << "Response header: \n" << m_responseHeader.str();
 
@@ -86,8 +86,6 @@ void ResponseBuilder::resetBuilder()
  */
 void ResponseBuilder::appendStatusLine(const HTTPRequest& request)
 {
-	if (request.hasCGI && (m_responseBody.find("HTTP/1.1 ") != std::string::npos))
-		return;
 	m_responseHeader << "HTTP/1.1 " << request.httpStatus << ' '
 					 << webutils::statusCodeToReasonPhrase(request.httpStatus) << "\r\n";
 }
@@ -108,53 +106,73 @@ void ResponseBuilder::appendHeaders(const HTTPRequest& request)
 {
 	if (!m_responseBody.empty()) {
 		// Content-Type
-		m_responseHeader << "Content-Type: " << getMIMEType(webutils::getFileExtension(request.targetResource))
-						 << "\r\n";
+		if (m_responseHeaders.find("Content-Type") != m_responseHeaders.end()) {
+			m_responseHeader << "Content-Type: " << m_responseHeaders.at("Content-Type") << "\r\n";
+			m_responseHeaders.erase("Content-Type");
+		}
+		else
+			m_responseHeader << "Content-Type: " << getMIMEType(webutils::getFileExtension(request.targetResource))
+							 << "\r\n";
 		// Content-Length
-		m_responseHeader << "Content-Length: " << m_responseBody.length() << "\r\n";
+		if (m_responseHeaders.find("Content-Length") != m_responseHeaders.end()) {
+			m_responseHeader << "Content-Length: " << m_responseHeaders.at("Content-Length") << "\r\n";
+			m_responseHeaders.erase("Content-Length");
+		}
+		else
+			m_responseHeader << "Content-Length: " << m_responseBody.length() << "\r\n";
 	}
+
+	// Various headers from response
+	for (std::map<std::string, std::string>::const_iterator iter = m_responseHeaders.begin();
+		 iter != m_responseHeaders.end(); ++iter)
+		m_responseHeader << iter->first << ": " << iter->second << "\r\n";
+	
 	// Server
 	m_responseHeader << "Server: TriHard\r\n";
+
 	// Date
 	m_responseHeader << "Date: " << webutils::getGMTString(time(0), "%a, %d %b %Y %H:%M:%S GMT") << "\r\n";
+
 	// Location
 	std::map<std::string, std::string>::const_iterator iter = request.headers.find("location");
 	if (iter != request.headers.end()) {
 		m_responseHeader << "Location: " << iter->second << "\r\n";
 	}
+
 	// Connection
 	if (request.shallCloseConnection)
 		m_responseHeader << "Connection: close\r\n";
 	else
 		m_responseHeader << "Connection: keep-alive\r\n";
+	
 	// Delimiter
 	m_responseHeader << "\r\n";
 }
 
-/**
- * @brief Append headers to the response for CGI.
- *
- * The following headers are appended:
- * Content-Length: Length of the response body.
- * Server: TriHard.
- * Date: Current date in GMT.
- * Location: Target resource if status is StatusMovedPermanently.
- * @param request HTTP request.
- */
-void ResponseBuilder::appendHeadersCGI(const HTTPRequest& request)
-{
-	// Content-Length
-	m_responseHeader << "Content-Length: " << m_responseBody.length() << "\r\n";
-	// Server
-	m_responseHeader << "Server: TriHard\r\n";
-	// Date
-	m_responseHeader << "Date: " << webutils::getGMTString(time(0), "%a, %d %b %Y %H:%M:%S GMT") << "\r\n";
-	// Connection
-	if (request.shallCloseConnection)
-		m_responseHeader << "Connection: close\r\n";
-	else
-		m_responseHeader << "Connection: keep-alive\r\n";
-}
+// /**
+//  * @brief Append headers to the response for CGI.
+//  *
+//  * The following headers are appended:
+//  * Content-Length: Length of the response body.
+//  * Server: TriHard.
+//  * Date: Current date in GMT.
+//  * Location: Target resource if status is StatusMovedPermanently.
+//  * @param request HTTP request.
+//  */
+// void ResponseBuilder::appendHeadersCGI(const HTTPRequest& request)
+// {
+// 	// Content-Length
+// 	m_responseHeader << "Content-Length: " << m_responseBody.length() << "\r\n";
+// 	// Server
+// 	m_responseHeader << "Server: TriHard\r\n";
+// 	// Date
+// 	m_responseHeader << "Date: " << webutils::getGMTString(time(0), "%a, %d %b %Y %H:%M:%S GMT") << "\r\n";
+// 	// Connection
+// 	if (request.shallCloseConnection)
+// 		m_responseHeader << "Connection: close\r\n";
+// 	else
+// 		m_responseHeader << "Connection: keep-alive\r\n";
+// }
 
 /**
  * @brief Init the MIME types map.
@@ -204,7 +222,6 @@ void ResponseBuilder::parseResponseBody(HTTPRequest& request)
 	std::vector<std::string> statusIdentifiers;
 	statusIdentifiers.push_back("Status");
 	statusIdentifiers.push_back(httpString);
-	
 
 	for (std::vector<std::string>::const_iterator iter = statusIdentifiers.begin(); iter != statusIdentifiers.end();
 		 ++iter) {
@@ -225,29 +242,30 @@ void ResponseBuilder::parseResponseBody(HTTPRequest& request)
 	}
 
 	size_t posHeadersEnd = m_responseBody.find("\r\n\r\n");
-	if (posHeadersEnd != std::string::npos) {
-		std::string headers = m_responseBody.substr(posStatusEnd, posHeadersEnd - posStatusEnd);
-		size_t lineStart = 0;
-        size_t lineEnd = headers.find("\r\n");
-        
-        while (lineEnd != std::string::npos) {
-            std::string header = headers.substr(lineStart, lineEnd - lineStart);
-            
-            std::string headerName;
-            std::string headerValue;
-            const std::size_t delimiterPos = header.find_first_of(':');
-            if (delimiterPos != std::string::npos) {
-                headerName = header.substr(0, delimiterPos);
-                headerValue = header.substr(delimiterPos + 1);
-                headerValue = webutils::trimLeadingWhitespaces(headerValue);
-                webutils::trimTrailingWhiteSpaces(headerValue);
-                m_responseHeaders[headerName] = headerValue;
-                LOG_DEBUG << "Parsed response header: " << headerName << " -> " << headerValue;
-            }
 
-            lineStart = lineEnd + 2;
-            lineEnd = headers.find("\r\n", lineStart);
-        }
+	if (posHeadersEnd != std::string::npos) {
+		// Include one CRLF at the end of last header line
+		std::string headers = m_responseBody.substr(posStatusEnd, posHeadersEnd - posStatusEnd + 2);
+		size_t lineStart = 0;
+		size_t lineEnd = headers.find("\r\n");
+
+		while (lineEnd != std::string::npos) {
+			std::string header = headers.substr(lineStart, lineEnd - lineStart);
+
+			std::string headerName;
+			std::string headerValue;
+			const std::size_t delimiterPos = header.find_first_of(':');
+			if (delimiterPos != std::string::npos) {
+				headerName = header.substr(0, delimiterPos);
+				headerValue = header.substr(delimiterPos + 1);
+				headerValue = webutils::trimLeadingWhitespaces(headerValue);
+				webutils::trimTrailingWhiteSpaces(headerValue);
+				m_responseHeaders[headerName] = headerValue;
+				LOG_DEBUG << "Parsed response header: " << headerName << " -> " << headerValue;
+			}
+
+			lineStart = lineEnd + 2;
+			lineEnd = headers.find("\r\n", lineStart);
+		}
 	}
 }
-
