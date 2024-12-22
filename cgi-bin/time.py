3cb6d5898fd5e58621de23fc522d1bbdaac28624
Sirui <sirui.qiu01@gmail.com>
2024-12-22 21:46:42 +0000

    test: add headers

--- cgi-bin/time.py
+++ cgi-bin/time.py
@@ -3,7 +3,10 @@
 import datetime
 
 print("HTTP/1.1 200 OK\r\n", end="")
-print("Content-Type: text/html\r\n\r\n", end="")
+print("Content-Type: text/html\r\n", end="")
+print("Connection: close\r\n", end="")
+print("Cache-Control: no-cache\r\n", end="")
+print("Pragma: no-cache\r\n\r\n", end="")
 print("<html>")
 print("<head>")
 print(datetime.datetime.strftime(datetime.datetime.now(), "<h1>  %H:%M:%S </h1>"))
