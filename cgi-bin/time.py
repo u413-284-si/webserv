3b2ecc9ce58dfdce720c88292572bde6b0353be8
Sirui <sirui.qiu01@gmail.com>
2024-12-22 20:56:58 +0000

    fix: print format of time.py

--- cgi-bin/time.py
+++ cgi-bin/time.py
@@ -2,8 +2,8 @@
 
 import datetime
 
-print("HTTP/1.1 200 OK\r\n")
-print("Content-Type: text/html\r\n\r\n")
+print("HTTP/1.1 200 OK\r\n", end="")
+print("Content-Type: text/html\r\n\r\n", end="")
 print("<html>")
 print("<head>")
 print(datetime.datetime.strftime(datetime.datetime.now(), "<h1>  %H:%M:%S </h1>"))
