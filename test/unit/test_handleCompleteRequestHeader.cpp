2f13ce5f43cb56440d84920d86da60dfe05e99e6
Gabriel Wolf <gwolf@student.42vienna.com>
2024-12-29 12:50:44 +0000

    feat: add tests for return and CGI

--- test/unit/test_handleCompleteRequestHeader.cpp
+++ test/unit/test_handleCompleteRequestHeader.cpp
@@ -1,14 +1,29 @@
 #include "test_helpers.hpp"
 
+using ::testing::DoAll;
 using ::testing::Return;
+using ::testing::SetArgReferee;
+using ::testing::SetArrayArgument;
 
 class HandleCompleteRequestHeaderTest : public ServerTestBase {
 protected:
 	HandleCompleteRequestHeaderTest()
 	{
+		ON_CALL(m_epollWrapper, addEvent).WillByDefault(Return(true));
 		ON_CALL(m_epollWrapper, modifyEvent).WillByDefault(Return(true));
 
-		m_connection.m_timeSinceLastEvent = 0;
+		Location location;
+		location.path = "/redirect";
+		location.returns = std::make_pair(StatusMovedPermanently, "/secret");
+		m_configFile.servers[0].locations.emplace_back(location);
+
+		Location location2;
+		location2.path = "/cgi-bin";
+		location2.cgiPath = "/usr/bin/bash";
+		location2.cgiExt = ".sh";
+		location2.allowedMethods[MethodPost] = true;
+		m_configFile.servers[0].locations.emplace_back(location2);
+
 		m_connection.m_status = Connection::ReceiveHeader;
 	}
 	~HandleCompleteRequestHeaderTest() override { }
@@ -45,9 +60,10 @@ TEST_F(HandleCompleteRequestHeaderTest, NotAllowedMethod)
 
 TEST_F(HandleCompleteRequestHeaderTest, POSTRequest)
 {
-	EXPECT_CALL(m_fileSystemOps, checkFileType).WillOnce(Return(FileSystemOps::FileRegular));
+	EXPECT_CALL(m_fileSystemOps, checkFileType).WillOnce(Return(FileSystemOps::FileNotExist));
 
-	m_connection.m_buffer.assign("POST / HTTP/1.1\r\nHost:example.com\r\nContent-Length:12\r\n\r\nThis is body");
+	m_connection.m_buffer.assign("POST /new.txt "
+								 "HTTP/1.1\r\nHost:example.com\r\nContent-Length:12\r\n\r\nThis is body");
 	m_configFile.servers[0].locations[0].allowedMethods[MethodPost] = true;
 
 	handleCompleteRequestHeader(m_server, m_dummyFd, m_connection);
@@ -74,4 +90,73 @@ TEST_F(HandleCompleteRequestHeaderTest, ConfigFileRandomlyDestroyed)
 
 	EXPECT_EQ(m_connection.m_request.httpStatus, StatusOK);
 	EXPECT_EQ(m_connection.m_status, Connection::Closed);
+}
+
+TEST_F(HandleCompleteRequestHeaderTest, GETRequestHitsLocationWithReturn)
+{
+	EXPECT_CALL(m_epollWrapper, modifyEvent);
+
+	m_connection.m_buffer.assign("GET /redirect HTTP/1.1\r\nHost:example.com\r\n\r\n");
+
+	handleCompleteRequestHeader(m_server, m_dummyFd, m_connection);
+
+	EXPECT_EQ(m_connection.m_request.httpStatus, StatusMovedPermanently);
+	EXPECT_EQ(m_connection.m_status, Connection::BuildResponse);
+}
+
+TEST_F(HandleCompleteRequestHeaderTest, GETRequestWithCGIhasError)
+{
+	EXPECT_CALL(m_fileSystemOps, checkFileType).WillOnce(Return(FileSystemOps::FileRegular));
+	EXPECT_CALL(m_processOps, pipeProcess).WillOnce(Return(-1));
+	EXPECT_CALL(m_epollWrapper, modifyEvent);
+
+	m_connection.m_buffer.assign("GET /cgi-bin/helloWorld.sh HTTP/1.1\r\nHost:example.com\r\n\r\n");
+
+	handleCompleteRequestHeader(m_server, m_dummyFd, m_connection);
+
+	EXPECT_EQ(m_connection.m_request.httpStatus, StatusInternalServerError);
+	EXPECT_FALSE(m_connection.m_request.hasCGI);
+	EXPECT_EQ(m_connection.m_status, Connection::BuildResponse);
+}
+
+TEST_F(HandleCompleteRequestHeaderTest, GETRequestWithCGI)
+{
+	int dummyPipeFds[2] = { 11, 21 };
+	pid_t dummyPid = 1234;
+
+	EXPECT_CALL(m_fileSystemOps, checkFileType).WillOnce(Return(FileSystemOps::FileRegular));
+	EXPECT_CALL(m_processOps, pipeProcess)
+		.WillOnce(DoAll(SetArrayArgument<0>(dummyPipeFds, dummyPipeFds + 2), Return(0)));
+	EXPECT_CALL(m_processOps, forkProcess).WillOnce(DoAll(SetArgReferee<0>(dummyPid), Return(0)));
+	EXPECT_CALL(m_epollWrapper, addEvent);
+
+	m_connection.m_buffer.assign("GET /cgi-bin/helloWorld.sh HTTP/1.1\r\nHost:example.com\r\n\r\n");
+
+	handleCompleteRequestHeader(m_server, m_dummyFd, m_connection);
+
+	EXPECT_EQ(m_connection.m_request.httpStatus, StatusOK);
+	EXPECT_EQ(m_connection.m_status, Connection::ReceiveFromCGI);
+}
+
+TEST_F(HandleCompleteRequestHeaderTest, POSTRequestWithCGIhasError)
+{
+	int dummyPipeFds[2] = { 11, 21 };
+	int dummyPipeFds2[2] = { 12, 22 };
+	pid_t dummyPid = 1234;
+
+	EXPECT_CALL(m_fileSystemOps, checkFileType).WillOnce(Return(FileSystemOps::FileRegular));
+	EXPECT_CALL(m_processOps, pipeProcess)
+		.WillOnce(DoAll(SetArrayArgument<0>(dummyPipeFds2, dummyPipeFds2 + 2), Return(0)))
+		.WillOnce(DoAll(SetArrayArgument<0>(dummyPipeFds, dummyPipeFds + 2), Return(0)));
+	EXPECT_CALL(m_processOps, forkProcess).WillOnce(DoAll(SetArgReferee<0>(dummyPid), Return(0)));
+	EXPECT_CALL(m_epollWrapper, addEvent).WillOnce(Return(false));
+
+	m_connection.m_buffer.assign(
+		"POST /cgi-bin/upperCase.sh HTTP/1.1\r\nHost:example.com\r\n\r\nContent-Length:12\r\n\r\nThis is body");
+
+	handleCompleteRequestHeader(m_server, m_dummyFd, m_connection);
+
+	EXPECT_EQ(m_connection.m_request.httpStatus, StatusInternalServerError);
+	EXPECT_FALSE(m_connection.m_request.hasCGI);
+	EXPECT_EQ(m_connection.m_status, Connection::BuildResponse);
 }
