b47678f613e0813df6911fc0181c9374d30472de
Gabriel Wolf <gwolf@student.42vienna.com>
2024-12-29 14:40:54 +0000

    feat: add tests for fullread of cgi

--- test/unit/test_connectionReceiveFromCGI.cpp
+++ test/unit/test_connectionReceiveFromCGI.cpp
@@ -4,20 +4,21 @@
 using ::testing::_;
 using ::testing::DoAll;
 using ::testing::Return;
+using ::testing::SetArgPointee;
 using ::testing::SetArrayArgument;
 
 class ConnectionReceiveFromCGITest : public ServerTestBase {
 protected:
 	ConnectionReceiveFromCGITest()
 	{
+		ON_CALL(m_epollWrapper, removeEvent).WillByDefault(Return());
+		ON_CALL(m_epollWrapper, addEvent).WillByDefault(Return(true));
+
 		connection.m_status = Connection::ReceiveFromCGI;
-		connection.m_pipeFromCGIReadEnd = pipefd[0];
-
-		ON_CALL(m_epollWrapper, addEvent)
-			.WillByDefault(Return(true));
-
-		ON_CALL(m_epollWrapper, removeEvent)
-			.WillByDefault(Return());
+		connection.m_request.hasCGI = true;
+		connection.m_pipeFromCGIReadEnd = 11;
+		connection.m_pipeToCGIWriteEnd = 12;
+		connection.m_cgiPid = 1234;
 	}
 	~ConnectionReceiveFromCGITest() override { }
 
@@ -28,15 +29,17 @@ protected:
 	Connection connection = Connection(serverSock, clientSocket, dummyFd, m_configFile.servers);
 	int pipefd[2];
 
-    void SetUp() override {
-        // Create a pipe
-        pipe(pipefd);
-    }
+	void SetUp() override
+	{
+		// Create a pipe
+		pipe(pipefd);
+	}
 
-    void TearDown() override {
-        close(pipefd[0]);
-        close(pipefd[1]);
-    }
+	void TearDown() override
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+	}
 };
 
 TEST_F(ConnectionReceiveFromCGITest, ReadError)
@@ -56,13 +59,11 @@ TEST_F(ConnectionReceiveFromCGITest, PartialRead)
 {
 	// Arrange
 	const char* response = "CGI response";
-    const ssize_t responseSize = strlen(response);
+	const ssize_t responseSize = strlen(response);
 
 	EXPECT_CALL(m_processOps, readProcess)
-        .Times(1)
-		.WillOnce(DoAll(
-			SetArrayArgument<1>(response, response + responseSize),
-			Return(responseSize)));
+		.Times(1)
+		.WillOnce(DoAll(SetArrayArgument<1>(response, response + responseSize), Return(responseSize)));
 
 	// Act
 	connectionReceiveFromCGI(m_server, pipefd[0], connection);
@@ -70,4 +71,85 @@ TEST_F(ConnectionReceiveFromCGITest, PartialRead)
 	// Assert
 	EXPECT_EQ(connection.m_request.body, response);
 	EXPECT_EQ(connection.m_status, Connection::ReceiveFromCGI);
+}
+
+TEST_F(ConnectionReceiveFromCGITest, FullReadWaitpidError)
+{
+	// Arrange
+	EXPECT_CALL(m_processOps, readProcess).WillOnce(Return(0));
+	EXPECT_CALL(m_processOps, waitForProcess).WillOnce(DoAll(SetArgPointee<1>(123), Return(-1)));
+
+	// Act
+	connectionReceiveFromCGI(m_server, pipefd[0], connection);
+
+	// Assert
+	EXPECT_EQ(connection.m_status, Connection::BuildResponse);
+	EXPECT_EQ(connection.m_pipeFromCGIReadEnd, -1);
+	EXPECT_EQ(connection.m_pipeToCGIWriteEnd, -1);
+	EXPECT_EQ(connection.m_request.httpStatus, StatusInternalServerError);
+	EXPECT_FALSE(connection.m_request.hasCGI);
+}
+
+TEST_F(ConnectionReceiveFromCGITest, FullReadChildExitSuccess)
+{
+	// Arrange
+	// Status Code is 0 = SUCCESS
+	const int exitCode = 0;
+	// Set the lower 8 bits of wstatus (exit code) to the desired exit value.
+	// Set bits 8–15 of wstatus to zero to indicate normal termination.
+	const int wstatus = (exitCode & 0xFF) << 8;
+	EXPECT_CALL(m_processOps, readProcess).WillOnce(Return(0));
+	EXPECT_CALL(m_processOps, waitForProcess).WillOnce(DoAll(SetArgPointee<1>(wstatus), Return(0)));
+
+	// Act
+	connectionReceiveFromCGI(m_server, pipefd[0], connection);
+
+	// Assert
+	EXPECT_EQ(connection.m_status, Connection::BuildResponse);
+	EXPECT_EQ(connection.m_pipeFromCGIReadEnd, -1);
+	EXPECT_EQ(connection.m_pipeToCGIWriteEnd, -1);
+}
+
+TEST_F(ConnectionReceiveFromCGITest, FullReadChildExitFailure)
+{
+	// Arrange
+	// Status Code is non 0 = FAILURE
+	const int exitCode = 42;
+	// Set the lower 8 bits of wstatus (exit code) to the desired exit value.
+	// Set bits 8–15 of wstatus to zero to indicate normal termination.
+	const int wstatus = (exitCode & 0xFF) << 8;
+	EXPECT_CALL(m_processOps, readProcess).WillOnce(Return(0));
+	EXPECT_CALL(m_processOps, waitForProcess).WillOnce(DoAll(SetArgPointee<1>(wstatus), Return(0)));
+
+	// Act
+	connectionReceiveFromCGI(m_server, pipefd[0], connection);
+
+	// Assert
+	EXPECT_EQ(connection.m_status, Connection::BuildResponse);
+	EXPECT_EQ(connection.m_pipeFromCGIReadEnd, -1);
+	EXPECT_EQ(connection.m_pipeToCGIWriteEnd, -1);
+	EXPECT_EQ(connection.m_request.httpStatus, StatusInternalServerError);
+	EXPECT_FALSE(connection.m_request.hasCGI);
+}
+
+TEST_F(ConnectionReceiveFromCGITest, FullReadChildReceivedSignal)
+{
+	// Arrange
+	// Status Code is non 0 = FAILURE
+	const int signalNumber = SIGKILL;;
+	// The lower 7 bits of wstatus (bits 0–6) hold the signal number.
+	// Bit 7 should be 0 (indicating no core dump).
+	const int wstatus = signalNumber & 0x7F;
+	EXPECT_CALL(m_processOps, readProcess).WillOnce(Return(0));
+	EXPECT_CALL(m_processOps, waitForProcess).WillOnce(DoAll(SetArgPointee<1>(wstatus), Return(0)));
+
+	// Act
+	connectionReceiveFromCGI(m_server, pipefd[0], connection);
+
+	// Assert
+	EXPECT_EQ(connection.m_status, Connection::BuildResponse);
+	EXPECT_EQ(connection.m_pipeFromCGIReadEnd, -1);
+	EXPECT_EQ(connection.m_pipeToCGIWriteEnd, -1);
+	EXPECT_EQ(connection.m_request.httpStatus, StatusInternalServerError);
+	EXPECT_FALSE(connection.m_request.hasCGI);
 }
