868e7142ef6cb4e5a57184060b49e053dd5fc9c9
Gabriel Wolf <gwolf@student.42vienna.com>
2024-12-31 09:16:28 +0000

    feat: implement make_request function

--- test/integration/server_response/test_GET.py
+++ test/integration/server_response/test_GET.py
@@ -1,86 +1,98 @@
 # This module is for succesful GET requests
 
-import requests
 import os
 
-def test_GET_simple():
+def test_GET_simple(make_request):
     print("Request for /index.html")
-    response = requests.get("http://localhost:8080/index.html")
+    url = "http://localhost:8080/index.html"
+    response = make_request(url)
     file_path = "/workspaces/webserv/html/index.html"
     file_size = os.path.getsize(file_path)
     assert int(response.headers["content-length"]) == file_size
     assert response.status_code == 200
 
-def test_GET_index_file():
+def test_GET_index_file(make_request):
     print("Request for /")
-    response = requests.get("http://127.0.0.1:8080")
+    url = "http://127.0.0.1:8080"
+    response = make_request(url)
     file_path = "/workspaces/webserv/html/index.html"
     file_size = os.path.getsize(file_path)
     assert int(response.headers["content-length"]) == file_size
     assert response.status_code == 200
 
-def test_GET_directory_listing():
+def test_GET_directory_listing(make_request):
     print("Request for /directory/")
-    response = requests.get("http://localhost:8080/directory/")
+    url = "http://localhost:8080/directory/"
+    response = make_request(url)
     heading = "Index of /workspaces/webserv/html/directory/"
     assert heading in response.text
     assert response.status_code == 200
 
-def test_GET_directory_redirect():
+def test_GET_directory_redirect(make_request):
     print("Request for /directory")
-    response = requests.get("http://localhost:8080/directory", allow_redirects=False)
+    url = "http://localhost:8080/directory"
+    response = make_request(url)
 # does not work right now
 #    assert response.headers["location"] == "/directory/"
-    assert response.status_code == 301
-
-def test_GET_percent_encoded():
-    print("Request for /images/grüne äpfel.jpg")
-    response = requests.get("http://localhost:8080/images/grüne äpfel.jpg")
     assert response.status_code == 200
 
-def test_GET_location_with_alias():
+def test_GET_percent_encoded(make_request):
+    print("Request for /images/grüne äpfel.jpg")
+    url ="http://localhost:8080/images/grüne äpfel.jpg"
+    response = make_request(url)
+    assert response.status_code == 200
+
+def test_GET_location_with_alias(make_request):
     print("Request for /alias/cat.jpg")
-    response = requests.get("http://localhost:8080/alias/cat.jpg")
+    url = "http://localhost:8080/alias/cat.jpg"
+    response = make_request(url)
     file_path = "/workspaces/webserv/html/images/cat.jpg"
     file_size = os.path.getsize(file_path)
     assert int(response.headers["content-length"]) == file_size
     assert response.status_code == 200
 
-def test_GET_directory_forbidden():
+def test_GET_directory_forbidden(make_request):
     print("Request for /secret/")
-    response = requests.get("http://localhost:8080/secret/")
+    url = "http://localhost:8080/secret/"
+    response = make_request(url)
     assert response.status_code == 403
 
-def test_GET_simple_redirect():
+def test_GET_simple_redirect(make_request):
     print("Request for /redirect")
-    response = requests.get("http://localhost:8080/redirect")
+    url = "http://localhost:8080/redirect"
+    response = make_request(url)
     assert response.history[0].status_code == 301
     assert response.history[0].headers["Location"] == "/secret"
     assert response.status_code == 403
 
-def test_GET_return_forbidden_error_page_returns_also():
+def test_GET_return_forbidden_error_page_returns_also(make_request):
     print("Request for /another")
-    response = requests.get("http://localhost:8080/another")
+    url = "http://localhost:8080/another"
+    response = make_request(url)
     assert response.status_code == 403
     assert "Return Message" in response.text
 
-def test_GET_missing_indices():
+def test_GET_missing_indices(make_request):
     print("Request for /missingIndex/")
-    response = requests.get("http://localhost:8080/missingIndex/")
+    url = "http://localhost:8080/missingIndex/"
+    response = make_request(url)
     assert response.status_code == 403
 
-def test_max_recurision():
+def test_max_recurision(make_request):
     print("Request for /recursion/")
-    response = requests.get("http://localhost:8080/recursion/")
+    url = "http://localhost:8080/recursion/"
+    response = make_request(url)
     assert response.status_code == 500
 
-def test_character_device():
+def test_character_device(make_request):
     print("Request for /tty")
-    response = requests.get("http://localhost:8080/tty")
+    url = "http://localhost:8080/tty"
+    response = make_request(url)
     assert response.status_code == 500
 
-def test_GET_empty_return_directive():
+def test_GET_empty_return_directive(make_request):
     print("Request for /empty")
-    response = requests.get("http://localhost:8080/empty")
+    url = "http://localhost:8080/empty"
+    response = make_request(url)
     assert response.status_code == 200
     assert len(response.content) == 0
