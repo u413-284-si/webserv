1af83a3ecfeb9c7f653cb96dfe413d01a260c0d7
Gabriel Wolf <gwolf@student.42vienna.com>
2024-12-31 14:41:25 +0000

    feat: add invalid tests

--- test/integration/server_response/test_invalid.py
+++ test/integration/server_response/test_invalid.py
@@ -21,72 +21,90 @@ def test_invalid_method_not_implemented():
         assert response["status_code"] == 501
 
 def test_invalid_missing_host_header():
-
-    # Create a socket connection
     with socket.create_connection((host, port)) as sock:
-        # Craft a malformed HTTP request
         request = b"GET / HTTP/1.1\r\n\r\n"
         sock.sendall(request)
 
-        # Receive the response
         response = parse_http_response(sock)
         assert response["status_code"] == 400
 
 def test_invalid_percent_encoding_invalid_char():
-
-    # Create a socket connection
     with socket.create_connection((host, port)) as sock:
-        # %00 is NUL char
         request = b"GET /search%00maschine HTTP/1.1\r\nHost: localhost\r\n\r\n"
         sock.sendall(request)
 
-        # Receive the response
         response = parse_http_response(sock)
         assert response["status_code"] == 400
 
 def test_invalid_percent_encoding_incomplete():
-
-    # Create a socket connection
     with socket.create_connection((host, port)) as sock:
-        # %00 is NUL char
         request = b"GET /search%3 HTTP/1.1\r\nHost: localhost\r\n\r\n"
         sock.sendall(request)
 
-        # Receive the response
         response = parse_http_response(sock)
         assert response["status_code"] == 400
 
 def test_invalid_percent_encoding_non_hex():
-
-    # Create a socket connection
     with socket.create_connection((host, port)) as sock:
-        # %00 is NUL char
         request = b"GET /search%1$ HTTP/1.1\r\nHost: localhost\r\n\r\n"
         sock.sendall(request)
 
-        # Receive the response
         response = parse_http_response(sock)
         assert response["status_code"] == 400
 
 def test_invalid_directory_traversal():
-    # Create a socket connection
     with socket.create_connection((host, port)) as sock:
-        # Craft HTTP request with directory traversal try
         request = b"GET /../../../etc/passwd HTTP/1.1\r\nHost: localhost\r\n\r\n"
         sock.sendall(request)
 
-        # Receive the response
         response = parse_http_response(sock)
         assert response["status_code"] == 400
 
 # Does not work, gets misinterpreted as method not implemented
 # def test_invalid_no_request_line():
-#     # Create a socket connection
 #     with socket.create_connection((host, port)) as sock:
-#         # Empty request line
 #         request = b"\r\n\r\n"
 #         sock.sendall(request)
-
-#         # Receive the response
+#
 #         response = parse_http_response(sock)
 #         assert response["status_code"] == 400
+
+def test_invalid_request_line_does_not_start_with_slash():
+    with socket.create_connection((host, port)) as sock:
+        request = b"GET hello.html HTTP/1.1\r\nHost: localhost\r\n\r\n"
+        sock.sendall(request)
+
+        response = parse_http_response(sock)
+        assert response["status_code"] == 400
+
+def test_invalid_not_allowed_char():
+    with socket.create_connection((host, port)) as sock:
+        request = b"GET /hello\x01world HTTP/1.1\r\nHost: localhost\r\n\r\n"
+        sock.sendall(request)
+
+        response = parse_http_response(sock)
+        assert response["status_code"] == 400
+
+def test_invalid_not_allowed_char():
+    with socket.create_connection((host, port)) as sock:
+        request = b"GET /hello\x01world HTTP/1.1\r\nHost: localhost\r\n\r\n"
+        sock.sendall(request)
+
+        response = parse_http_response(sock)
+        assert response["status_code"] == 400
+
+def test_invalid_header_name_ends_in_space():
+    with socket.create_connection((host, port)) as sock:
+        request = b"GET / HTTP/1.1\r\nHost: localhost\r\nInvalid : not allowed\r\n\r\n"
+        sock.sendall(request)
+
+        response = parse_http_response(sock)
+        assert response["status_code"] == 400
+
+def test_invalid_char_in_header_name():
+    with socket.create_connection((host, port)) as sock:
+        request = b"GET / HTTP/1.1\r\nHost: localhost\r\nInva/lid: not allowed\r\n\r\n"
+        sock.sendall(request)
+
+        response = parse_http_response(sock)
+        assert response["status_code"] == 400
