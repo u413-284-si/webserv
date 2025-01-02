a51109dd11e41aa76639f17e155aecba987e6dac
Gabriel Wolf <gwolf@student.42vienna.com>
2025-01-02 11:15:49 +0000

    chore: deactivate clang-format for default_config_path

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,11 @@
  */
 #define STRINGIZE(s) #s
 
+// clang-format off
 #ifndef DEFAULT_CONFIG_PATH
-#define DEFAULT_CONFIG_PATH ./ config_files / standard_config.conf
+#define DEFAULT_CONFIG_PATH ./config_files/standard_config.conf
 #endif
+// clang-format on
 
 int main(const int argc, const char* argv[])
 {
