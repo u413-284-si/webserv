df6a23cbd38ccb58fbfdcad48838edc375c546db
Gabriel Wolf <gwolf@student.42vienna.com>
2025-01-12 11:32:41 +0000

    fix: reactivate test forced_shutdown

--- test/integration/server_management/test_signal.py
+++ test/integration/server_management/test_signal.py
@@ -31,28 +31,28 @@ def test_signal_graceful_shutdown(request):
     exit_status = server_process.returncode
     assert exit_status == 0
 
-# def test_signal_forced_shutdown(request):
+def test_signal_forced_shutdown(request):
 
-#     server_executable = request.config.getoption("--server-executable")
-#     config_file = config_file = request.config.getoption("--config-file")
+    server_executable = request.config.getoption("--server-executable")
+    config_file = request.config.getoption("--config-file")
 
-#     # Check if the user passed the --with-coverage option
-#     with_coverage = request.config.getoption("--with-coverage")
+    # Check if the user passed the --with-coverage option
+    with_coverage = request.config.getoption("--with-coverage")
 
-#     kcov_output_dir = request.config.getoption("--kcov-output-dir")
-#     kcov_excl_path = request.config.getoption("--kcov-excl-path")
+    kcov_output_dir = request.config.getoption("--kcov-output-dir")
+    kcov_excl_path = request.config.getoption("--kcov-excl-path")
 
-#     server_process = start_server(server_executable, config_file, with_coverage, kcov_output_dir, kcov_excl_path)
+    server_process = start_server(server_executable, config_file, with_coverage, kcov_output_dir, kcov_excl_path)
 
-#     wait_for_startup()
+    wait_for_startup(server_process)
 
-#     if with_coverage:
-#         pid = int(subprocess.check_output(["pgrep", "webserv"]).decode().strip())
-#     else:
-#         pid = server_process.pid
+    if with_coverage:
+        pid = int(subprocess.check_output(["pgrep", "webserv"]).decode().strip())
+    else:
+        pid = server_process.pid
 
-#     print("Sending SIGINT...")
-#     os.kill(pid, signal.SIGINT)
-#     server_process.wait()
-#     exit_status = server_process.returncode
-#     assert exit_status != 0
+    print("Sending SIGINT...")
+    os.kill(pid, signal.SIGINT)
+    server_process.wait()
+    exit_status = server_process.returncode
+    assert exit_status == 0
