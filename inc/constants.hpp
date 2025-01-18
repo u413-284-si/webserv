437243beeba56bb608d8e103d5f44759aac23b1c
QCHR1581 <chris1581.at@gmail.com>
2025-01-18 12:01:47 +0000

    feat: added g_oneGigabyte

--- inc/constants.hpp
+++ inc/constants.hpp
@@ -10,5 +10,6 @@ namespace constants {
 extern const int g_decimalBase;
 extern const size_t g_oneKilobyte;
 extern const size_t g_oneMegabyte;
+extern const size_t g_oneGigabyte;
 
 } // constants
--- src/constants.cpp
+++ src/constants.cpp
@@ -5,5 +5,6 @@ namespace constants {
 const int g_decimalBase = 10;
 const size_t g_oneKilobyte = 1024;
 const size_t g_oneMegabyte = 1048576;
+const size_t g_oneGigabyte = 1073741824;
 
 } // constants
