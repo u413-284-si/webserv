97bbfef0277b15b1eb300393b794f48eed8b807c
Chris <94694991+QCHR1581@users.noreply.github.com>
2025-01-25 14:16:14 +0000

    fix: changed data type from int to size_t

--- inc/ConfigFileParser.hpp
+++ inc/ConfigFileParser.hpp
@@ -44,8 +44,8 @@ private:
 	size_t m_serverIndex;
 	std::vector<std::string> m_validServerDirectives;
 	size_t m_locationIndex;
-	int m_serverRootCount;
-	int m_locationRootCount;
+	size_t m_serverRootCount;
+	size_t m_locationRootCount;
 	std::vector<std::string> m_validLocationDirectives;
 	bool m_isDefaultLocationDefined;
 	static const char* const s_whitespace;
