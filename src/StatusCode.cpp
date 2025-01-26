7715fdf794e919531a0809aabe091a6593e39f56
Gabriel Wolf <gwolf@student.42vienna.com>
2025-01-26 10:14:34 +0000

    chore: update Doxygen

--- src/StatusCode.cpp
+++ src/StatusCode.cpp
@@ -1,5 +1,12 @@
 #include "StatusCode.hpp"
 
+/**
+ * @brief Converts a status code to a string.
+ *
+ * If the status code is not a known status code, it returns "0".
+ * @param statusCode Status code.
+ * @return std::string String representation of the status code.
+ */
 std::string statusCodeToString(statusCode statusCode)
 {
 	if (statusCode < NoStatus || statusCode > StatusNonSupportedVersion)
@@ -39,12 +46,13 @@ std::string statusCodeToString(statusCode statusCode)
 	case StatusNonSupportedVersion:
 		return ("505");
 	}
+	return ("0");
 }
 
 /**
  * @brief Returns reason phrase for a given status code.
  *
- * In case of NoStatus returns the string "NO STATUS CODE".
+ * If the status code is not known returns the string "NO STATUS CODE".
  * @param statusCode Status code.
  * @return std::string Reason phrase.
  */
@@ -87,38 +95,13 @@ std::string statusCodeToReasonPhrase(statusCode statusCode)
 	case StatusNonSupportedVersion:
 		return "HTTP Version Not Supported";
 	}
-}
-
-/**
- * @brief Check if a given status code is an error status code.
- *
- * An error status code is a 3xx, 4xx or 5xx status code.
- * @param statusCode Status code to check.
- * @return true If the status code is an error status code.
- * @return false If the status code is not an error status code.
- */
-bool isErrorStatus(statusCode statusCode) { return (statusCode >= StatusMovedPermanently); }
-
-/**
- * @brief Check if a given status code is a redirection.
- *
- * A redirection is a 3xx status code.
- * @param statusCode Status code to check.
- * @return true If the status code is a redirection.
- * @return false If the status code is not a redirection.
- */
-bool isRedirectionStatus(statusCode statusCode)
-{
-	return (statusCode >= StatusMovedPermanently && statusCode <= StatusPermanentRedirect);
+	return "NO STATUS CODE";
 }
 
 /**
  * @brief Converts a string to an HTTP status code.
  *
- * This function takes a string representation of an HTTP status code
- * and returns the corresponding statusCode enum value. If the string
- * does not match any known status code, it returns StatusBadRequest.
- *
+ * Expects the string to be exactly 3 numbers long. If the string is not a valid status code, it returns NoStatus.
  * @param str The string representation of the HTTP status code.
  * @return The corresponding statusCode enum value.
  */
@@ -154,6 +137,13 @@ statusCode stringToStatusCode(const std::string& str)
 	return (NoStatus);
 }
 
+/**
+ * @brief Extracts the status code from a status line.
+ *
+ * Extratcs from the first number found till the last number.
+ * @param statusLine The status line.
+ * @return The status code.
+ */
 statusCode extractStatusCode(const std::string& statusLine)
 {
 	size_t pos = statusLine.find_first_of("0123456789");
@@ -162,4 +152,27 @@ statusCode extractStatusCode(const std::string& statusLine)
 		return stringToStatusCode(statusCodeString);
 	}
 	return NoStatus;
+}
+
+/**
+ * @brief Check if a given status code is an error status code.
+ *
+ * An error status code is a 3xx, 4xx or 5xx status code.
+ * @param statusCode Status code to check.
+ * @return true If the status code is an error status code.
+ * @return false If the status code is not an error status code.
+ */
+bool isErrorStatus(statusCode statusCode) { return (statusCode >= StatusMovedPermanently); }
+
+/**
+ * @brief Check if a given status code is a redirection.
+ *
+ * A redirection is a 3xx status code.
+ * @param statusCode Status code to check.
+ * @return true If the status code is a redirection.
+ * @return false If the status code is not a redirection.
+ */
+bool isRedirectionStatus(statusCode statusCode)
+{
+	return (statusCode >= StatusMovedPermanently && statusCode <= StatusPermanentRedirect);
 }
