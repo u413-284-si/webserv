15d56c2aa1c2e3cb6630fd57d763591c90f1b4ff
Gabriel Wolf <gwolf@students.42vienna.com>
2025-01-27 13:00:31 +0000

    feat: add test for no permissions

--- test/integration/server_response/test_DELETE.py
+++ test/integration/server_response/test_DELETE.py
@@ -23,3 +23,18 @@ def test_DELETE_directory(test_path_cleanup):
     assert response.status_code == 403
     assert response.headers["content-type"] == "text/html"
     assert os.path.isdir(dir_path)
+
+def test_DELETE_no_permissions(test_path_cleanup, temp_permission_change):
+    existing_content = "You can't delete me"
+    file_path = "/workspaces/webserv/html/uploads/immune_to_delete.txt"
+    with open(file_path, "w") as file:
+        file.write(existing_content)
+    test_path_cleanup.append(file_path)
+    # ability to delete is determined by permissions of the directory containing the file.
+    dir_path = "/workspaces/webserv/html/uploads"
+    temp_permission_change(dir_path, 0o555)
+    url = "http://localhost:8080/uploads/immune_to_delete.txt"
+    response = make_request(url, method="DELETE")
+    assert response.status_code == 403
+    assert response.headers["content-type"] == "text/html"
+    
