4bb629d1727fd656e05670ab291ebdfeac27c823
Gabriel Wolf <gwolf@students.42vienna.com>
2025-01-27 14:41:42 +0000

    feat: add test for 302 found

--- test/integration/server_response/test_error3xx.py
+++ test/integration/server_response/test_error3xx.py
@@ -16,8 +16,14 @@ def test_3xx_return_redirect():
     assert response.status_code == 301
     assert response.headers["Location"] == "/secret"
 
-def test_3xx_permanent_redirect():
+def test_3xx_found():
     url = "http://localhost:8080/nevergonna"
     response = make_request(url, allow_redirects=False)
-    assert response.status_code == 308
+    assert response.status_code == 302
     assert response.headers["Location"] == "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+def test_3xx_permament_redirect():
+    url = "http://localhost:8080/newplace"
+    response = make_request(url, allow_redirects=False)
+    assert response.status_code == 308
+    assert response.headers["Location"] == "/nevergonna"
