10a77bb80223acc6c7983835ebf9ba317cf654b6
Gabriel Wolf <gwolf@students.42vienna.com>
2025-01-27 14:50:53 +0000

    feat: add test for method not supported

--- test/integration/server_response/test_error5xx.py
+++ test/integration/server_response/test_error5xx.py
@@ -23,7 +23,6 @@ def test_5xx_method_not_implemented():
 
         # Receive the response
         response = parse_http_response(sock)
-        print(response["status_code"])
         assert response["status_code"] == 501
 
 def test_5xx_cgi_with_no_header():
@@ -45,3 +44,11 @@ def test_5xx_cgi_with_invalid_header_connection():
     url = "http://localhost:8080/cgi-bin/invalid_header_connection.sh"
     response = make_request(url)
     assert response.status_code == 500
+
+def test_5xx_http_version_0_9():
+    with socket.create_connection((host, port)) as sock:
+        request = b"GET / HTTP/0.9\r\nHost: localhost\r\n\r\n"
+        sock.sendall(request)
+        response = parse_http_response(sock)
+        assert response["status_code"] == 505
+        assert response["headers"].get("connection") == "keep-alive"
